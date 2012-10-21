1fe7a879239770c409f195aae9503486786fb172
Jari Bakken <jari.bakken@gmail.com>
2012-10-21 18:49:48 +0200

    Fix path for `/bin/cat`

--- manifests/config/afterservice.pp
+++ manifests/config/afterservice.pp
@@ -38,7 +38,7 @@ class postgresql::config::afterservice(
         #  the password is correct (current), this command will exit with an exit code of 0,
         #  which will prevent the main command from running.
         unless      => "env PGPASSWORD=\"$postgres_password\" psql -h localhost -c 'select 1' > /dev/null",
-        path        => '/usr/bin:/usr/local/bin',
+        path        => '/usr/bin:/usr/local/bin:/bin',
     }
   }
 }
