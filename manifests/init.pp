ec0e159a1d6b0260d6717b387963615ce454a9f1
Hunter Haugen <h.haugen@gmail.com>
2012-12-19 16:50:45 -0800

    Update the base class to accept `version` parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,8 +7,6 @@
 #
 # Parameters:
 #   [*version*] - The postgresql version to install.
-#   [*package_name*]  - The name of the postgresql client package.
-#   [*ensure*] - the ensure parameter passed to the postgresql client package resource
 # Actions:
 #
 # Requires:
@@ -16,14 +14,9 @@
 # Sample Usage:
 #
 class postgresql (
-  $package_name   = $postgresql::params::client_package_name,
-  $package_ensure = 'present'
-) inherits postgresql::params {
-
-  package { 'postgresql-client':
-    ensure  => $package_ensure,
-    name    => $package_name,
-    tag    => 'postgresql',
+  $version = $::postgres_default_version,
+) {
+  class { 'postgresql::params':
+    version => $version,
   }
-
 }
