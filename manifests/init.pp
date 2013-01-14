1477d0350353a5560391b6db624067e8e62615d3
Chris Price <chris@puppetlabs.com>
2013-01-14 13:26:34 -0800

    Clean up docs for base class (init.pp)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,28 @@
 # Class: postgresql
 #
-#   This class installs postgresql client software.
+#   This is a base class that can be used to modify catalog-wide settings relating
+#   to the various types in class contained in the postgresql module.
 #
-#   *Note* don't forget to make sure to add any necessary yum or apt
-#   repositories if specifying a custom version.
+#   If you don't declare this class in your catalog, sensible defaults will
+#   be used.  However, if you choose to declare it, it needs to appear *before*
+#   any other types or classes from the postgresql module.
+#
+#   For examples, see the files in the `tests` directory; in particular,
+#    `/server-yum-postgresql-org.pp`.
 #
 # Parameters:
-#   [*version*] - The postgresql version to install.
+#   [*version*] - The postgresql version to install.  If not specified, the
+#                 module will use whatever version is the default for your
+#                 OS distro.
+#   [*manage_package_repo*] - This determines whether or not the module should
+#                 attempt to manage the postgres package repository for your
+#                 distro.  Defaults to `false`, but if set to `true`, it can
+#                 be used to set up the official postgres yum/apt package
+#                 repositories for you.
+#   [*package_source*] - This setting is only used if `manage_package_repo` is
+#                 set to `true`.  It determines which package repository should
+#                 be used to install the postgres packages.  Currently supported
+#                 values include `yum.postgresql.org`.
 # Actions:
 #
 # Requires:
