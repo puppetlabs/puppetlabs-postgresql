bd9bcf4cc2b65be09979da98f3411815c4ab0478
Flaper Fesp <flaper87@gmail.com>
2013-01-17 10:52:46 +0100

    Add support for psycopg installation

--- manifests/python.pp
+++ manifests/python.pp
@@ -1,6 +1,10 @@
 # Class: postgresql::python
 # This class installs the python libs for postgresql.
-
+#
+# Parameters:
+#   [*ensure*]       - ensure state for package.
+#                        can be specified as version.
+#   [*package_name*] - name of package
 class postgresql::python(
   $package_name   = $postgresql::params::python_package_name,
   $package_ensure = 'present'
