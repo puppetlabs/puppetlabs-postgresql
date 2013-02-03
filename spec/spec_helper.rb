e9370fee7bb1ee7d9fad17aa638b08279dda0ea6
Ken Barber <ken@bob.sh>
2013-02-03 18:25:53 +0100

    Add unit tests and travis-ci support
    This patch includes some very basic and initial unit testing using rspec-puppet
    and for the case of facts, just normal rspec.
    
    I've taken a very light approach here as rspec-puppet can be quite combinatorial
    when one gets carried away. For now I've just added basic compile failure
    detection effectively for classes and defined resources. As we continue to work
    on the code and find regressions this work can be expanded.
    
    For facts and functions I've also taken a basic approach for now.
    
    One little thing I did change, was the strange string that the fact returns
    when the default version is undefined. Instead of an error message I've just
    returned the string 'unknown' which is more in line with other facts I've seen
    in the wild, and to be quite honest 'unknown' is fairly self-explantory. Since
    a fact isn't an error reporting message this seemed more appropriate, and looked
    nicer in the rspec test.
    
    As far as travis-ci support, I've added the same configuration that @jmmcune
    came up with for stdlib which is pretty light and reasonable standard now we
    propogated that to 4 or so other modules in the puppetlabs/ namespace. It should
    work out of the box.
    
    Signed-off-by: Ken Barber <ken@bob.sh>

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'puppetlabs_spec_helper/module_spec_helper'
+
+RSpec.configure do |config|
+  config.before :each do
+    # Ensure that we don't accidentally cache facts and environment
+    # between test cases.
+    Facter::Util::Loader.any_instance.stubs(:load_all)
+    Facter.clear
+    Facter.clear_messages
+
+    # Store any environment variables away to be restored later
+    @old_env = {}
+    ENV.each_key {|k| @old_env[k] = ENV[k]}
+  end
+end
