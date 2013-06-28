fd9e2b5869fa6e6f5c129bd00edb8aa7d4b7e1ff
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-06-28 15:19:50 +0200

    Alter superuser, createdb, createrole, login, replication status and the connection limit of a role that already exists

--- manifests/role.pp
+++ manifests/role.pp
@@ -29,12 +29,6 @@ define postgresql::role(
 ) {
   include postgresql::params
 
-  Postgresql_psql {
-    psql_user    => $postgresql::params::user,
-    psql_group   => $postgresql::params::group,
-    psql_path    => $postgresql::params::psql_path,
-  }
-
   $login_sql       = $login       ? { true => 'LOGIN'       , default => 'NOLOGIN' }
   $createrole_sql  = $createrole  ? { true => 'CREATEROLE'  , default => 'NOCREATEROLE' }
   $createdb_sql    = $createdb    ? { true => 'CREATEDB'    , default => 'NOCREATEDB' }
@@ -46,10 +40,44 @@ define postgresql::role(
     $password_sql = ""
   }
 
-  # TODO: FIXME: Will not correct the superuser / createdb / createrole / login / replication status nor the connection limit of a role that already exists
-  postgresql_psql {"CREATE ROLE \"${username}\" ${password_sql} ${login_sql} ${createrole_sql} ${createdb_sql} ${superuser_sql} ${replication_sql} CONNECTION LIMIT ${connection_limit}":
-    db        => $db,
-    psql_user => $postgresql::params::user,
-    unless    => "SELECT rolname FROM pg_roles WHERE rolname='${username}'",
+  Postgresql_psql {
+    db         => $db,
+    psql_user  => $postgresql::params::user,
+    psql_group => $postgresql::params::group,
+    psql_path  => $postgresql::params::psql_path,
+    require    => Postgresql_psql["CREATE ROLE \"${username}\" ${password_sql} ${login_sql} ${createrole_sql} ${createdb_sql} ${superuser_sql} ${replication_sql} CONNECTION LIMIT ${connection_limit}"],
   }
+
+  # TODO: FIXME: Will not correct the password of a role that already exists
+  postgresql_psql {"CREATE ROLE \"${username}\" ${password_sql} ${login_sql} ${createrole_sql} ${createdb_sql} ${superuser_sql} ${replication_sql} CONNECTION LIMIT ${connection_limit}":
+    unless  => "SELECT rolname FROM pg_roles WHERE rolname='${username}'",
+    require => undef,
+  }
+
+  postgresql_psql {"ALTER ROLE \"${username}\" ${superuser_sql}":
+    unless => "SELECT rolname FROM pg_roles WHERE rolname='${username}' and rolsuper=${superuser}",
+  }
+
+  postgresql_psql {"ALTER ROLE \"${username}\" ${createdb_sql}":
+    unless => "SELECT rolname FROM pg_roles WHERE rolname='${username}' and rolcreatedb=${createdb}",
+  }
+
+  postgresql_psql {"ALTER ROLE \"${username}\" ${createrole_sql}":
+    unless => "SELECT rolname FROM pg_roles WHERE rolname='${username}' and rolcreaterole=${createrole}",
+  }
+
+  postgresql_psql {"ALTER ROLE \"${username}\" ${login_sql}":
+    unless => "SELECT rolname FROM pg_roles WHERE rolname='${username}' and rolcanlogin=${login}",
+  }
+
+  if(versioncmp($postgresql::params::version, '9.0') >= 0) {
+    postgresql_psql {"ALTER ROLE \"${username}\" ${replication_sql}":
+      unless => "SELECT rolname FROM pg_roles WHERE rolname='${username}' and rolreplication=${replication}",
+    }
+  }
+
+  postgresql_psql {"ALTER ROLE \"${username}\" CONNECTION LIMIT ${connection_limit}":
+    unless => "SELECT rolname FROM pg_roles WHERE rolname='${username}' and rolconnlimit=${connection_limit}",
+  }
+
 }
