f9693c39aa9914d6cd6af3e91d1440d4b14d351d
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-06-28 16:49:58 +0200

    Add user alteration system test

--- spec/system/install_spec.rb
+++ spec/system/install_spec.rb
@@ -204,6 +204,41 @@ describe 'install:' do
         r.exit_code.should == 0
       end
     end
+
+    it 'should idempotently alter a user who can log in' do
+      pp = <<-EOS
+        $user = "postgresql_test_user"
+        $password = "postgresql_test_password2"
+
+        include postgresql::server
+
+        # Since we are not testing pg_hba or any of that, make a local user for ident auth
+        user { $user:
+          ensure => present,
+        }
+
+        postgresql::database_user { $user:
+          password_hash => postgresql_password($user, $password),
+          require  => [ Class['postgresql::server'],
+                        User[$user] ],
+        }
+      EOS
+
+      puppet_apply(pp) do |r|
+        r.exit_code.should_not == 1
+      end
+
+      puppet_apply(pp) do |r|
+        r.exit_code.should be_zero
+      end
+
+      # Check that the user can log in
+      psql('--command="select datname from pg_database" postgres', 'postgresql_test_user2') do |r|
+        r.stdout.should =~ /template1/
+        r.stderr.should be_empty
+        r.exit_code.should == 0
+      end
+    end
   end
 
   describe 'postgresql::grant' do
