8919359bcadba6ea477ed0647ac52739130426c2
Hunter Haugen <h.haugen@gmail.com>
2013-07-19 11:49:44 -0700

    Use boolean for refreshonly
    Puppet::Type supports `:boolean => true` to create a
    `resource.<property>?` method that is true or false based on the
    `:true`, `:false`, `true`, or `false` value of the parameter.
    
    This will allow accurate assesment of the refreshonly attribute for
    testing.

--- lib/puppet/type/postgresql_psql.rb
+++ lib/puppet/type/postgresql_psql.rb
@@ -24,7 +24,7 @@ Puppet::Type.newtype(:postgresql_psql) do
       # method, and then inside of the body of 'sync' we can tell
       # whether or not we're refreshing.
 
-      if ((@resource[:refreshonly] == :false) || refreshing)
+      if (!@resource.refreshonly? || refreshing)
         # If we're not in 'refreshonly' mode, or we're not currently
         # refreshing, then we just call the parent method.
         super()
@@ -69,10 +69,11 @@ Puppet::Type.newtype(:postgresql_psql) do
     defaultto("/tmp")
   end
 
-  newparam(:refreshonly) do
+  newparam(:refreshonly, :boolean => true) do
     desc "If 'true', then the SQL will only be executed via a notify/subscribe event."
 
     defaultto(:false)
+    newvalues(:true, :false)
   end
 
   def refresh()
