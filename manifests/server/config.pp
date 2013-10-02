6f614b0b3735e1b2f9e701677f3fc6f85e852184
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-10-02 14:43:21 +0200

    Add a parameter to (un)manage pg_hba.conf

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -12,6 +12,7 @@ class postgresql::server::config {
   $user                       = $postgresql::server::user
   $group                      = $postgresql::server::group
   $version                    = $postgresql::server::version
+  $manage_pg_hba_conf         = $postgresql::server::manage_pg_hba_conf
 
   File {
     owner => $user,
@@ -19,72 +20,75 @@ class postgresql::server::config {
   }
 
   if ($ensure == 'present' or $ensure == true) {
-    # Prepare the main pg_hba file
-    include concat::setup
-    concat { $pg_hba_conf_path:
-      owner  => 0,
-      group  => $group,
-      mode   => '0640',
-      warn   => true,
-      notify => Class['postgresql::server::reload'],
-    }
 
-    if $pg_hba_conf_defaults {
-      Postgresql::Server::Pg_hba_rule {
-        database => 'all',
-        user => 'all',
+    if ($manage_pg_hba_conf == true) {
+      # Prepare the main pg_hba file
+      include concat::setup
+      concat { $pg_hba_conf_path:
+        owner  => 0,
+        group  => $group,
+        mode   => '0640',
+        warn   => true,
+        notify => Class['postgresql::server::reload'],
       }
 
-      # Lets setup the base rules
-      $local_auth_option = $version ? {
-        '8.1'   => 'sameuser',
-        default => undef,
-      }
-      postgresql::server::pg_hba_rule { 'local access as postgres user':
-        type        => 'local',
-        user        => $user,
-        auth_method => 'ident',
-        auth_option => $local_auth_option,
-        order       => '001',
-      }
-      postgresql::server::pg_hba_rule { 'local access to database with same name':
-        type        => 'local',
-        auth_method => 'ident',
-        auth_option => $local_auth_option,
-        order       => '002',
-      }
-      postgresql::server::pg_hba_rule { 'deny access to postgresql user':
-        type        => 'host',
-        user        => $user,
-        address     => $ip_mask_deny_postgres_user,
-        auth_method => 'reject',
-        order       => '003',
-      }
+      if $pg_hba_conf_defaults {
+        Postgresql::Server::Pg_hba_rule {
+          database => 'all',
+          user => 'all',
+        }
 
-      # ipv4acls are passed as an array of rule strings, here we transform them
-      # into a resources hash, and pass the result to create_resources
-      $ipv4acl_resources = postgresql_acls_to_resources_hash($ipv4acls,
+        # Lets setup the base rules
+        $local_auth_option = $version ? {
+          '8.1'   => 'sameuser',
+          default => undef,
+        }
+        postgresql::server::pg_hba_rule { 'local access as postgres user':
+          type        => 'local',
+          user        => $user,
+          auth_method => 'ident',
+          auth_option => $local_auth_option,
+          order       => '001',
+        }
+        postgresql::server::pg_hba_rule { 'local access to database with same name':
+          type        => 'local',
+          auth_method => 'ident',
+          auth_option => $local_auth_option,
+          order       => '002',
+        }
+        postgresql::server::pg_hba_rule { 'deny access to postgresql user':
+          type        => 'host',
+          user        => $user,
+          address     => $ip_mask_deny_postgres_user,
+          auth_method => 'reject',
+          order       => '003',
+        }
+
+        # ipv4acls are passed as an array of rule strings, here we transform
+        # them into a resources hash, and pass the result to create_resources
+        $ipv4acl_resources = postgresql_acls_to_resources_hash($ipv4acls,
         'ipv4acls', 10)
-      create_resources('postgresql::server::pg_hba_rule', $ipv4acl_resources)
+        create_resources('postgresql::server::pg_hba_rule', $ipv4acl_resources)
 
-      postgresql::server::pg_hba_rule { 'allow access to all users':
-        type        => 'host',
-        address     => $ip_mask_allow_all_users,
-        auth_method => 'md5',
-        order       => '100',
-      }
-      postgresql::server::pg_hba_rule { 'allow access to ipv6 localhost':
-        type        => 'host',
-        address     => '::1/128',
-        auth_method => 'md5',
-        order       => '101',
-      }
+        postgresql::server::pg_hba_rule { 'allow access to all users':
+          type        => 'host',
+          address     => $ip_mask_allow_all_users,
+          auth_method => 'md5',
+          order       => '100',
+        }
+        postgresql::server::pg_hba_rule { 'allow access to ipv6 localhost':
+          type        => 'host',
+          address     => '::1/128',
+          auth_method => 'md5',
+          order       => '101',
+        }
 
-      # ipv6acls are passed as an array of rule strings, here we transform them
-      # into a resources hash, and pass the result to create_resources
-      $ipv6acl_resources = postgresql_acls_to_resources_hash($ipv6acls,
+        # ipv6acls are passed as an array of rule strings, here we transform
+        # them into a resources hash, and pass the result to create_resources
+        $ipv6acl_resources = postgresql_acls_to_resources_hash($ipv6acls,
         'ipv6acls', 102)
-      create_resources('postgresql::server::pg_hba_rule', $ipv6acl_resources)
+        create_resources('postgresql::server::pg_hba_rule', $ipv6acl_resources)
+      }
     }
 
     # We must set a "listen_addresses" line in the postgresql.conf if we
