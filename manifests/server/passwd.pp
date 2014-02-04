eaa665b342b1ade2a7f0721efdc205436d5400c3
root <root@debian.gogrid.net>
2014-02-04 14:53:13 -0700

    Add support for returning the system user password back to NULL.

--- manifests/server/passwd.pp
+++ manifests/server/passwd.pp
@@ -13,21 +13,40 @@ class postgresql::server::passwd {
       #  the default for pg_hba.conf.
       $escaped = postgresql_escape($postgres_password)
       $env = "env PGPASSWORD='${postgres_password}'"
-      exec { 'set_postgres_postgrespw':
-        # This command works w/no password because we run it as postgres system
-        # user
-        command     => "psql -c 'ALTER ROLE \"${user}\" PASSWORD ${escaped}'",
-        user        => $user,
-        group       => $group,
-        logoutput   => true,
-        cwd         => '/tmp',
-        # With this command we're passing -h to force TCP authentication, which
-        # does require a password.  We specify the password via the PGPASSWORD
-        # environment variable. If the password is correct (current), this
-        # command will exit with an exit code of 0, which will prevent the main
-        # command from running.
-        unless      => "${env} psql -h localhost -c 'select 1' > /dev/null",
-        path        => '/usr/bin:/usr/local/bin:/bin',
+      if ($escaped == '$$false$$') {
+        notify { "System user password being (re)set to NULL.": }
+        exec { 'set_postgres_postgrespw':
+          # This command works w/no password because we run it as postgres system
+          # user
+          command     => "psql -c 'ALTER ROLE \"${user}\" PASSWORD NULL'",
+          user        => $user,
+          group       => $group,
+          logoutput   => true,
+          cwd         => '/tmp',
+          path        => '/usr/bin:/usr/local/bin:/bin',
+          # An 'unless' clause would be useful but there does not seem to be
+          # any non-invasive way to determine if the system password is null,
+          # analogous to the way the clause works for the populated password
+          # condition in the else{} block below.
+        }
+      }
+      else {
+        exec { 'set_postgres_postgrespw':
+          # This command works w/no password because we run it as postgres system
+          # user
+          command     => "psql -c 'ALTER ROLE \"${user}\" PASSWORD ${escaped}'",
+          user        => $user,
+          group       => $group,
+          logoutput   => true,
+          cwd         => '/tmp',
+          # With this command we're passing -h to force TCP authentication, which
+          # does require a password.  We specify the password via the PGPASSWORD
+          # environment variable. If the password is correct (current), this
+          # command will exit with an exit code of 0, which will prevent the main
+          # command from running.
+          unless      => "${env} psql -h localhost -c 'select 1' > /dev/null",
+          path        => '/usr/bin:/usr/local/bin:/bin',
+        }
       }
     }
   }
