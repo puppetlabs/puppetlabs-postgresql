8b0053e2a7e20c957c134434b4ea6dfdd724baf7
Igor Galić <i.galic@brainsware.org>
2014-07-04 14:35:32 +0200

    defined type for creating database schemas
    This defined type helps create database schemas, and assign them to an
    `owner`. It is closely modeled after Postgresql::Server::Tablespace.
    It uses PostgreSQL's builtin IF NOT EXISTS to guarantee idempotency.
    (>= 9.3, else it checks pg_namespace).
    
    n.b.: This defined type *requires* that a `db` is passed. This is a
    concious design decision, since we find it rather useless to create such
    schemas in the default `postgres` database, and if *were* useful, one
    can always "over-specify".
    
    This addresses MODULES-1098.

---
+++ manifests/server/schema.pp
@@ -0,0 +1,44 @@
+# This defined types creates database schemas. See README.md for more details.
+define postgresql::server::schema(
+  $db,
+  $owner  = undef,
+  $schema = $title,
+) {
+  $user      = $postgresql::server::user
+  $group     = $postgresql::server::group
+  $port      = $postgresql::server::port
+  $psql_path = $postgresql::server::psql_path
+  $version   = $postgresql::server::version
+
+  Postgresql_psql {
+    db         => $db,
+    psql_user  => $user,
+    psql_group => $group,
+    psql_path  => $psql_path,
+    port       => $port,
+  }
+
+  $schema_title   = "Create Schema '${schema}'"
+  $authorization = $owner? {
+    undef   => '',
+    default => "AUTHORIZATION \"${owner}\"",
+  }
+
+  if(versioncmp($version, '9.3') >= 0) {
+    $schema_command = "CREATE SCHEMA IF NOT EXISTS \"${schema}\" ${authorization}"
+    $unless         = undef
+  } else {
+    $schema_command = "CREATE SCHEMA \"${schema}\" ${authorization}"
+    $unless         = "SELECT nspname FROM pg_namespace WHERE nspname='${schema}'"
+  }
+
+  postgresql_psql { $schema_title:
+    command => $schema_command,
+    unless  => $unless,
+    require => Class['postgresql::server'],
+  }
+
+  if($owner != undef and defined(Postgresql::Server::Role[$owner])) {
+    Postgresql::Server::Role[$owner]->Postgresql_psql[$schema_title]
+  }
+}
