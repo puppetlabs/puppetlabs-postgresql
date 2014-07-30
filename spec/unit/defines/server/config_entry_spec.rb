2c451b680b2f228f130bd42030b956f20a2fb42e
Ashley Penney <ashley.penney@puppetlabs.com>
2014-07-30 17:06:32 -0400

    Make sure this works on Fedora as well as RHEL7.

--- spec/unit/defines/server/config_entry_spec.rb
+++ spec/unit/defines/server/config_entry_spec.rb
@@ -27,5 +27,25 @@ describe 'postgresql::server::config_entry', :type => :define do
     let(:params) { { :ensure => 'present'} }
     it { should contain_postgresql__server__config_entry('config_entry') }
   end
+
+  context 'fedora 19' do
+    let :facts do
+      {
+        :osfamily => 'RedHat',
+        :operatingsystem => 'Fedora',
+        :operatingsystemrelease => '19',
+        :kernel => 'Linux',
+        :concat_basedir => tmpfilename('contrib'),
+        :id => 'root',
+        :path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      }
+    end
+    let(:params) {{ :ensure => 'present', :name => 'port', :value => '5432' }}
+
+    it 'stops postgresql and changes the port' do
+      is_expected.to contain_file('systemd-port-override')
+      is_expected.to contain_exec('restart-systemd')
+    end
+  end
 end
 
