3fd7a325e946ae29acd519f669930ec91b0c2333
Hugh Esco <hesco@campaignfoundations.com>
2014-09-22 21:22:29 -0400

    Basically working now, next to add unless tests, and tests of success.

--- manifests/server/ssl_certificate.pp
+++ manifests/server/ssl_certificate.pp
@@ -1,47 +1,50 @@
 
+# http://www.postgresql.org/docs/9.3/static/ssl-tcp.html
 # http://blog.roomthirteen.de/2013/01/07/solved-installing-postgresql-on-ubuntu-12-04/
+# http://stackoverflow.com/questions/4294689/how-to-generate-a-key-with-passphrase-from-the-command-line
 
-class postgresql::server::ssl_certificate {
+class postgresql::server::ssl_certificate (
 
-  $days         = $postgresql::server::cerificate_days
-  $country      = $postgresql::server::cerificate_country
-  $state        = $postgresql::server::certificate_state
-  $locality     = $postgresql::server::certificate_locality
-  $organization = $postgresql::server::certificate_organization
-  $common_name  = $postgresql::server::certificate_common_name
-  $email        = $postgresql::server::certificate_email
+  $force                    = 0,
+  $datadir                  = $postgresql::server::datadir,
+  $certificate_days         = $postgresql::server::certificate_days,
+  $certificate_country      = $postgresql::server::certificate_country,
+  $certificate_state        = $postgresql::server::certificate_state,
+  $certificate_locality     = $postgresql::server::certificate_locality,
+  $certificate_organization = $postgresql::server::certificate_organization,
+  $certificate_common_name  = $postgresql::server::certificate_common_name,
+  $certificate_email        = $postgresql::server::certificate_email,
+
+){
 
   # /C=CA/ST=British Columbia/L=Comox/O=TheBrain.ca/CN=thebrain.ca/emailAddress=info@thebrain.ca'
-  $subject = "/C=${country}/ST=${state}/L=${locality}/O=${organization}/CN=${common_name}/emailAddress=${email}" 
+  $certificate_subject = "/C=${certificate_country}/ST=${certificate_state}/L=${certificate_locality}/O=${certificate_organization}/CN=${certificate_common_name}/emailAddress=${certificate_email}" 
 
   $err_prefix = "Module postgresql::server::ssl_certificate unable to create server certificate: please specify a value for postgresql::server::certificate_"
-  if ($days == undef) { fail("${err_prefix}days") }
-  if ($country == undef) { fail("${err_prefix}country") }
-  if ($state == undef) { fail("${err_prefix}state") }
-  if ($locality == undef) { fail("${err_prefix}locality") }
-  if ($organization == undef) { fail("${err_prefix}organization") }
-  if ($common_name == undef) { fail("${err_prefix}common_name") }
-  if ($email == undef) { fail("${err_prefix}email") }
+  if ($certificate_days == undef) { fail("$certificate_{err_prefix}days") }
+  if ($certificate_country == undef) { fail("$certificate_{err_prefix}country") }
+  if ($certificate_state == undef) { fail("$certificate_{err_prefix}state") }
+  if ($certificate_locality == undef) { fail("$certificate_{err_prefix}locality") }
+  if ($certificate_organization == undef) { fail("$certificate_{err_prefix}organization") }
+  if ($certificate_common_name == undef) { fail("$certificate_{err_prefix}common_name") }
+  if ($certificate_email == undef) { fail("$certificate_{err_prefix}email") }
 
-  file { [ "$postgresql::params::postgresql_conf_path/server.crt",
-           "$postgresql::params::postgresql_conf_path/server.key" ]:
-    ensure => absent,
+  notify { "debug_postgresql::server::ssl_certificate":
+    message => "The postgresql data path is at: $datadir",
+  }
+
+  file { "${datadir}/generate_self_signed_pg_server_certificate.sh":
+    source => 'puppet:///modules/postgresql/generate_self_signed_pg_server_certificate.sh',
   }
 
   exec { 'generate_self_signed_pg_server_certificate':
-        cwd => "$postgresql::params::postgresql_conf_path",
-    command => "/usr/bin/openssl genrsa -des3 -passout pass:puppetlabs-postgresql -out trash.key 1024
-                /usr/bin/openssl rsa -passin pass:puppetlabs-postgresql -in trash.key -out trash.key 
-                /usr/bin/openssl req -new -key trash.key -days $days -out trash.cert -x509 $subject 
-                /bin/cp server.crt root.crt ",
-  }
-
-  file { "$postgresql::params::postgresql_conf_path/server.key":
-    ensure => file,
-      mode => '0400',
-      user => $postgresql::server::user,
-     group => $postgresql::server::group, 
+    command => "${datadir}/generate_self_signed_pg_server_certificate.sh $datadir $postgresql::server::user $postgresql::server::group '$certificate_subject' $certificate_days $force",
   }
 
 }
+
+# root@samora007:~# openssl x509 -in /var/lib/postgresql/9.3/main/server.crt -noout -text | grep Issuer 
+#         Issuer: C=US, ST=GA, L=Atlanta, O=YMDPartnersLLC, CN=samora007.yourmessagedelivered.com/emailAddress=hesco@yourmessagedelivered.com
+# root@samora007:~# openssl x509 -in /var/lib/postgresql/9.3/main/server.crt -noout -text | grep 'Not Before'
+#             Not Before: Sep 23 01:14:03 2014 GMT
 
