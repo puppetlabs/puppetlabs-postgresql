c6683194f6aec1d2fe80bfc8e9315bfab28431e9
Hugh Esco <hesco@campaignfoundations.com>
2014-09-24 01:59:38 -0400

    Resolve puppet-lint issues with postgresql::server::ssl_certificate

--- manifests/server/ssl_certificate.pp
+++ manifests/server/ssl_certificate.pp
@@ -18,19 +18,19 @@ class postgresql::server::ssl_certificate (
 ){
 
   # /C=CA/ST=British Columbia/L=Comox/O=TheBrain.ca/CN=thebrain.ca/emailAddress=info@thebrain.ca'
-  $certificate_subject = "/C=${certificate_country}/ST=${certificate_state}/L=${certificate_locality}/O=${certificate_organization}/CN=${certificate_common_name}/emailAddress=${certificate_email}" 
+  $certificate_subject = "/C=${certificate_country}/ST=${certificate_state}/L=${certificate_locality}/O=${certificate_organization}/CN=${certificate_common_name}/emailAddress=${certificate_email}"
 
-  $err_prefix = "Module postgresql::server::ssl_certificate unable to create server certificate: please specify a value for postgresql::server::certificate_"
-  if ($certificate_days == undef) { fail("$certificate_{err_prefix}days") }
-  if ($certificate_country == undef) { fail("$certificate_{err_prefix}country") }
-  if ($certificate_state == undef) { fail("$certificate_{err_prefix}state") }
-  if ($certificate_locality == undef) { fail("$certificate_{err_prefix}locality") }
-  if ($certificate_organization == undef) { fail("$certificate_{err_prefix}organization") }
-  if ($certificate_common_name == undef) { fail("$certificate_{err_prefix}common_name") }
-  if ($certificate_email == undef) { fail("$certificate_{err_prefix}email") }
+  $err_prefix = 'Module postgresql::server::ssl_certificate unable to create server certificate: please specify a value for postgresql::server::certificate_'
+  if ($certificate_days == undef) { fail("${err_prefix}days") }
+  if ($certificate_country == undef) { fail("${err_prefix}country") }
+  if ($certificate_state == undef) { fail("${err_prefix}state") }
+  if ($certificate_locality == undef) { fail("${err_prefix}locality") }
+  if ($certificate_organization == undef) { fail("${err_prefix}organization") }
+  if ($certificate_common_name == undef) { fail("${err_prefix}common_name") }
+  if ($certificate_email == undef) { fail("${err_prefix}email") }
 
-  notify { "debug_postgresql::server::ssl_certificate":
-    message => "The postgresql data path is at: $datadir",
+  notify { 'debug_postgresql::server::ssl_certificate':
+    message => "The postgresql data path is at: ${datadir}",
   }
 
   file { "${datadir}/validate_self_signed_ssl_certificate.pl":
@@ -42,7 +42,7 @@ class postgresql::server::ssl_certificate (
   }
 
   exec { 'generate_self_signed_pg_server_certificate':
-    command => "${datadir}/generate_self_signed_pg_server_certificate.sh $datadir $postgresql::server::user $postgresql::server::group '$certificate_subject' $certificate_days $force",
+    command => "${datadir}/generate_self_signed_pg_server_certificate.sh ${datadir} ${postgresql::server::user} ${postgresql::server::group} '${certificate_subject}' ${certificate_days} ${force}",
   }
 
 }
