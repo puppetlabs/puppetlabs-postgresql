53048211d02b1e758eb7b9c89aedb132566eaa72
David Caro <dcaroest@redhat.com>
2014-10-27 19:23:29 +0100

    Added fc21 default version

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -51,6 +51,7 @@ class postgresql::globals (
   $default_version = $::osfamily ? {
     /^(RedHat|Linux)/ => $::operatingsystem ? {
       'Fedora' => $::operatingsystemrelease ? {
+        /^(21)$/ => '9.3',
         /^(18|19|20)$/ => '9.2',
         /^(17)$/ => '9.1',
         default => undef,
