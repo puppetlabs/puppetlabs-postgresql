a46bddec076022a35c1c63bd33b7ec2a9603f684
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-12-05 11:27:32 +0100

    Support ensure=absent

--- manifests/server/extension.pp
+++ manifests/server/extension.pp
@@ -1,21 +1,48 @@
 # Activate an extension on a postgresql database
 define postgresql::server::extension (
   $database,
+  $ensure = 'present',
   $package_name = undef,
-  $package_ensure = 'present',
+  $package_ensure = undef,
 ) {
+  case $ensure {
+    'present': {
+      $command = "CREATE EXTENSION ${name}"
+      $unless_comp = '='
+      $package_require = undef
+      $package_before = Postgresql_psql["Add ${title} extension to ${database}"]
+    }
+
+    'absent': {
+      $command = "DROP EXTENSION ${name}"
+      $unless_comp = '!='
+      $package_require = Postgresql_psql["Add ${title} extension to ${database}"]
+      $package_before = undef
+    }
+
+    default: {
+      fail("Unknown value for ensure '${ensure}'.")
+    }
+  }
+
   postgresql_psql {"Add ${title} extension to ${database}":
     db      => $database,
-    command => "CREATE EXTENSION ${name}",
-    unless  => "SELECT extname FROM pg_extension WHERE extname = '${name}'",
+    command => $command,
+    unless  => "SELECT t.count FROM (SELECT count(extname) FROM pg_extension WHERE extname = '${name}') as t WHERE t.count ${unless_comp} 1",
     require => Postgresql::Server::Database[$database],
   }
 
   if $package_name {
+    $_package_ensure = $package_ensure ? {
+      undef   => $ensure,
+      default => $package_ensure,
+    }
+
     package { "Postgresql extension ${title}":
-      ensure => $package_ensure,
+      ensure => $_package_ensure,
       name   => $package_name,
-      before => Postgresql_psql["Add ${title} extension to ${database}"],
+      require => $package_require,
+      before => $package_before,
     }
   }
 }
--- spec/unit/defines/server/extension_spec.rb
+++ spec/unit/defines/server/extension_spec.rb
@@ -32,8 +32,8 @@ describe 'postgresql::server::extension', :type => :define do
       is_expected.to contain_postgresql_psql('Add postgis extension to template_postgis').with({
         :db      => 'template_postgis',
         :command => 'CREATE EXTENSION postgis',
-        :unless  => "SELECT extname FROM pg_extension WHERE extname = 'postgis'",
-      })
+        :unless  => "SELECT t.count FROM (SELECT count(extname) FROM pg_extension WHERE extname = 'postgis') as t WHERE t.count = 1",
+      }).that_requires('Postgresql::Server::Database[template_postgis]')
     }
   end
 
@@ -42,14 +42,60 @@ describe 'postgresql::server::extension', :type => :define do
       :package_name => 'postgis',
     }) }
 
-    it "should take a package_name optional argument" do
+    it { is_expected.to compile.with_all_deps }
 
-      is_expected.to compile.with_all_deps
-
+    it {
       is_expected.to contain_package('Postgresql extension postgis').with({
+        :ensure  => 'present',
+        :name    => 'postgis',
+      }).that_comes_before('Postgresql_psql[Add postgis extension to template_postgis]')
+    }
+  end
+
+  context "when ensuring absence" do
+    let (:params) { super().merge({
+      :ensure       => 'absent',
+      :package_name => 'postgis',
+    }) }
+
+    it { is_expected.to compile.with_all_deps }
+
+    it {
+      is_expected.to contain_postgresql_psql('Add postgis extension to template_postgis').with({
+        :db      => 'template_postgis',
+        :command => 'DROP EXTENSION postgis',
+        :unless  => "SELECT t.count FROM (SELECT count(extname) FROM pg_extension WHERE extname = 'postgis') as t WHERE t.count != 1",
+      }).that_requires('Postgresql::Server::Database[template_postgis]')
+    }
+
+    it {
+      is_expected.to contain_package('Postgresql extension postgis').with({
+        :ensure  => 'absent',
+        :name    => 'postgis',
+      })
+    }
+
+    context "when keeping package installed" do
+      let (:params) { super().merge({
+        :package_ensure => 'present',
+      }) }
+
+      it { is_expected.to compile.with_all_deps }
+
+      it {
+        is_expected.to contain_postgresql_psql('Add postgis extension to template_postgis').with({
+          :db      => 'template_postgis',
+          :command => 'DROP EXTENSION postgis',
+          :unless  => "SELECT t.count FROM (SELECT count(extname) FROM pg_extension WHERE extname = 'postgis') as t WHERE t.count != 1",
+        }).that_requires('Postgresql::Server::Database[template_postgis]')
+      }
+
+      it {
+        is_expected.to contain_package('Postgresql extension postgis').with({
           :ensure  => 'present',
           :name    => 'postgis',
-        })
+        }).that_requires('Postgresql_psql[Add postgis extension to template_postgis]')
+      }
     end
   end
 end
