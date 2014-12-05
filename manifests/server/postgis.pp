f50ea2c6b36706113c9cd28006599b5d5aa363d8
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-12-05 11:36:03 +0100

    Use ensure, package_ensure and package_name from postgresql::server::extension

--- manifests/server/postgis.pp
+++ manifests/server/postgis.pp
@@ -1,19 +1,14 @@
 # Install the postgis postgresql packaging. See README.md for more details.
 class postgresql::server::postgis (
   $package_name   = $postgresql::params::postgis_package_name,
-  $package_ensure = 'present',
+  $extension_ensure = 'present',
+  $package_ensure = undef,
   $template = false,
   $template_name = 'template_postgis',
   $base_template = 'template1',
   $extensions = ['postgis', 'postgis_topology'],
 ) inherits postgresql::params {
   validate_string($package_name)
-
-  package { 'postgresql-postgis':
-    ensure => $package_ensure,
-    name   => $package_name,
-    tag    => 'postgresql',
-  }
 
   if $template {
     postgresql::server::database { 'postgis template':
@@ -22,7 +17,10 @@ class postgresql::server::postgis (
       template   => $base_template,
     }
     postgresql::server::extension { $extensions:
-      database => $template_name,
+      database       => $template_name,
+      ensure         => $extension_ensure,
+      package_name   => $package_name,
+      package_ensure => $package_ensure,
     }
   }
 
