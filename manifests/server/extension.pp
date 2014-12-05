847da271025a9abd4fcf638af7e837f606784869
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-12-05 10:30:40 +0100

    Add package_name and package_ensure to postgresql::server::extension

--- manifests/server/extension.pp
+++ manifests/server/extension.pp
@@ -1,11 +1,20 @@
 # Activate an extension on a postgresql database
 define postgresql::server::extension (
   $database,
+  $package_name = undef,
+  $package_ensure = 'present',
 ) {
   postgresql_psql {"Add ${title} extension to ${database}":
     db      => $database,
     command => "CREATE EXTENSION ${name}",
     unless  => "SELECT extname FROM pg_extension WHERE extname = '${name}'",
     require => Postgresql::Server::Database[$database],
+  }
+
+  if $package_name {
+    package { "Postgresql extension ${title}":
+      ensure => $package_ensure,
+      name   => $package_name,
+    }
   }
 }
