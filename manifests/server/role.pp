df69a8ea241b50615274d94a3d3b26feb80aeeaf
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2015-03-29 13:28:55 +0200

    reorder environment and require parameter in modules/postgresql/manifest s/server/role.pp     to unbreak on OpenBSD puppet 3.7.4, with future parser, and ruby 2.1.5.
        otherwise error:
    
        Info: Loading facts
        Info: Loading facts
        Error: Could not retrieve catalog from remote server: Error 400 on SERVER: I
    nvalid parameter environment on Postgresql_psql[CREATE ROLE puppetdb ENCRYPTED P
    ASSWORD ****] at /etc/puppet/environments/production/modules/postgresql/manifest
    s/server/role.pp:46 on node puppetdb.srv.intern
        Warning: Not using cache on failed catalog
        Error: Could not retrieve catalog; skipping run

--- manifests/server/role.pp
+++ manifests/server/role.pp
@@ -46,8 +46,8 @@ define postgresql::server::role(
   postgresql_psql { "CREATE ROLE ${username} ENCRYPTED PASSWORD ****":
     command     => "CREATE ROLE \"${username}\" ${password_sql} ${login_sql} ${createrole_sql} ${createdb_sql} ${superuser_sql} ${replication_sql} CONNECTION LIMIT ${connection_limit}",
     unless      => "SELECT rolname FROM pg_roles WHERE rolname='${username}'",
-    require     => Class['Postgresql::Server'],
     environment => $environment,
+    require     => Class['Postgresql::Server'],
   }
 
   postgresql_psql {"ALTER ROLE \"${username}\" ${superuser_sql}":
