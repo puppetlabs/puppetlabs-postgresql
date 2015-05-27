6507fd2c122a1f6d2eb7e19e10ed1af89471b0df
Robert Hafner <tedivm@tedivm.com>
2015-05-27 16:51:55 -0700

    Switched to the 2.0 version of the apt module

--- manifests/repo/apt_postgresql_org.pp
+++ manifests/repo/apt_postgresql_org.pp
@@ -13,9 +13,14 @@ include ::apt
     location    => 'http://apt.postgresql.org/pub/repos/apt/',
     release     => "${::lsbdistcodename}-pgdg",
     repos       => "main ${postgresql::repo::version}",
-    key         => 'B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8',
-    key_source  => 'https://www.postgresql.org/media/keys/ACCC4CF8.asc',
-    include_src => false,
+    key         => {
+      id         => 'B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8',
+      key_source  => 'https://www.postgresql.org/media/keys/ACCC4CF8.asc',
+    },
+    include  => {
+      'src' => false,
+      'deb' => true,
+    },
   }
 
   Apt::Source['apt.postgresql.org']->Package<|tag == 'postgresql'|>
