82c6716d4845bde01a03c58b2123b3009c0d97ca
Nate Potter <ntpttr@gmail.com>
2016-07-09 13:53:34 -0700

    (MODULES-3247) Enable schema and database ownership change
    Currently postgresql::server::schema, postgresql::server::db and
    postgresql::server::database allow creating new schemas and
    databases if they don't already exist and assigning owners to
    them. This patch enables changing the owner of a schema or database
    that already exists if the change_ownership variable is set to true.

--- manifests/server/schema.pp
+++ manifests/server/schema.pp
@@ -17,6 +17,7 @@ define postgresql::server::schema(
   $owner  = undef,
   $schema = $title,
   $connect_settings = $postgresql::server::default_connect_settings,
+  $change_ownership = false,
 ) {
   $user      = $postgresql::server::user
   $group     = $postgresql::server::group
@@ -39,19 +40,33 @@ define postgresql::server::schema(
     connect_settings => $connect_settings,
   }
 
-  $schema_title   = "Create Schema '${title}'"
+  $schema_exists = "SELECT nspname FROM pg_namespace WHERE nspname='${schema}'"
   $authorization = $owner? {
     undef   => '',
     default => "AUTHORIZATION \"${owner}\"",
   }
 
-  $schema_command = "CREATE SCHEMA \"${schema}\" ${authorization}"
-  $unless         = "SELECT nspname FROM pg_namespace WHERE nspname='${schema}'"
-
-  postgresql_psql { $schema_title:
-    command => $schema_command,
-    unless  => $unless,
-    require => Class['postgresql::server'],
+  if $change_ownership {
+    # Change owner for existing schema
+    if !$owner {
+      fail('Must specify an owner to change schema ownership.')
+    }
+    $schema_title   = "Change owner of schema '${schema}' to ${owner}"
+    $schema_command = "ALTER SCHEMA \"${schema}\" OWNER TO ${owner}"
+    postgresql_psql { $schema_title:
+      command => $schema_command,
+      onlyif  => $schema_exists,
+      require => Class['postgresql::server'],
+    }
+  } else {
+    # Create a new schema
+    $schema_title   = "Create Schema '${title}'"
+    $schema_command = "CREATE SCHEMA \"${schema}\" ${authorization}"
+    postgresql_psql { $schema_title:
+      command => $schema_command,
+      unless  => $schema_exists,
+      require => Class['postgresql::server'],
+    }
   }
 
   if($owner != undef and defined(Postgresql::Server::Role[$owner])) {
