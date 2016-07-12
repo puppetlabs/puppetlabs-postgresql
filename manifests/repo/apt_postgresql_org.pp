3fe1e83678730256997d318c8276cae2a0394904
b4ldr <github@johnbond.org>
2016-07-12 19:11:46 +0100

    fix WARNING: is deprecated and will be removed in the next major release, please use = src = } instead

--- manifests/repo/apt_postgresql_org.pp
+++ manifests/repo/apt_postgresql_org.pp
@@ -10,12 +10,14 @@ include ::apt
     priority   => 500,
   }->
   apt::source { 'apt.postgresql.org':
-    location    => 'http://apt.postgresql.org/pub/repos/apt/',
-    release     => "${::lsbdistcodename}-pgdg",
-    repos       => "main ${postgresql::repo::version}",
-    key         => 'B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8',
-    key_source  => 'https://www.postgresql.org/media/keys/ACCC4CF8.asc',
-    include_src => false,
+    location   => 'http://apt.postgresql.org/pub/repos/apt/',
+    release    => "${::lsbdistcodename}-pgdg",
+    repos      => "main ${postgresql::repo::version}",
+    key        => 'B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8',
+    key_source => 'https://www.postgresql.org/media/keys/ACCC4CF8.asc',
+    include    => {
+      src      => false,
+    }
   }
 
   Apt::Source['apt.postgresql.org']->Package<|tag == 'postgresql'|>
