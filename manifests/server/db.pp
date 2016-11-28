b4948bd4741c617951e6b4a3b01bbb8a135787d5
Emre Hasegeli <emre@hasegeli.com>
2016-11-28 11:23:22 +0100

    Revert "Enable schema and database ownership change"
    This reverts commit 82c6716d4845bde01a03c58b2123b3009c0d97ca.  It will
    be added back with a cleaner design with the following commits.

--- manifests/server/db.pp
+++ manifests/server/db.pp
@@ -3,28 +3,26 @@
 define postgresql::server::db (
   $user,
   $password,
-  $comment          = undef,
-  $dbname           = $title,
-  $encoding         = $postgresql::server::encoding,
-  $locale           = $postgresql::server::locale,
-  $grant            = 'ALL',
-  $tablespace       = undef,
-  $template         = 'template0',
-  $istemplate       = false,
-  $owner            = undef,
-  $change_ownership = false,
+  $comment    = undef,
+  $dbname     = $title,
+  $encoding   = $postgresql::server::encoding,
+  $locale     = $postgresql::server::locale,
+  $grant      = 'ALL',
+  $tablespace = undef,
+  $template   = 'template0',
+  $istemplate = false,
+  $owner      = undef
 ) {
 
   if ! defined(Postgresql::Server::Database[$dbname]) {
     postgresql::server::database { $dbname:
-      comment          => $comment,
-      encoding         => $encoding,
-      tablespace       => $tablespace,
-      template         => $template,
-      locale           => $locale,
-      istemplate       => $istemplate,
-      owner            => $owner,
-      change_ownership => $change_ownership,
+      comment    => $comment,
+      encoding   => $encoding,
+      tablespace => $tablespace,
+      template   => $template,
+      locale     => $locale,
+      istemplate => $istemplate,
+      owner      => $owner,
     }
   }
 
