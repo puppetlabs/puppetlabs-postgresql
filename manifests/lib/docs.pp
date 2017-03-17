a038e74cdd63bd6fd076b323db39f23764dd60b9
Tim Meusel <tim@bastelfreak.de>
2017-03-17 13:08:37 +0100

    replace validate_* calls with datatypes in docs.pp

--- manifests/lib/docs.pp
+++ manifests/lib/docs.pp
@@ -1,11 +1,9 @@
 # This class installs the postgresql-docs See README.md for more
 # details.
 class postgresql::lib::docs (
-  $package_name   = $postgresql::params::docs_package_name,
+  String $package_name   = $postgresql::params::docs_package_name,
   $package_ensure = 'present',
 ) inherits postgresql::params {
-
-  validate_string($package_name)
 
   package { 'postgresql-docs':
     ensure => $package_ensure,
