38f57730a3c612a8b2ab2830b1101ecf1bbb63fd
Tim Meusel <tim@bastelfreak.de>
2017-03-17 13:09:14 +0100

    add datatypes to all params in docs.pp

--- manifests/lib/docs.pp
+++ manifests/lib/docs.pp
@@ -1,8 +1,8 @@
 # This class installs the postgresql-docs See README.md for more
 # details.
 class postgresql::lib::docs (
-  String $package_name   = $postgresql::params::docs_package_name,
-  $package_ensure = 'present',
+  String $package_name                                             = $postgresql::params::docs_package_name,
+  Enum['present', 'absent', 'latest', 'installed'] $package_ensure = 'present',
 ) inherits postgresql::params {
 
   package { 'postgresql-docs':
