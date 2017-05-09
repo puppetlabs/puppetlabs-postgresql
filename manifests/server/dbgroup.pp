6538f889a32ef1d6780ea0348e4fd243e95db3a7
Tom Robinson <trobinso@yelp.com>
2017-05-08 17:33:07 -0700

    Add dbgroup_spec, update syntax to provide arrays to pg_group UPDATE statement

--- manifests/server/dbgroup.pp
+++ manifests/server/dbgroup.pp
@@ -2,7 +2,7 @@
 define postgresql::server::dbgroup(
   $db               = $postgresql::server::default_database,
   $port             = undef, 
-  $groupmembers     = {},
+  $groupmembers     = [],
   $groupname        = $title,
   $dialect          = $postgresql::server::dialect,
   $connect_settings = undef,
@@ -38,14 +38,14 @@ define postgresql::server::dbgroup(
   }
 
   postgresql_psql { "${title}: CREATE GROUP ${groupname}":
-    command     => "CREATE GROUP \"${groupname}\"",
+    command     => "CREATE GROUP '${groupname}'",
     unless      => "SELECT 1 FROM pg_group WHERE groname = '${groupname}'",
     environment => [],
     require     => Class['Postgresql::Server'],
   }
 
-  postgresql_psql {"${title}: UPDATE pg_group SET grolist = '${groupmembers}' WHERE groname = '${groupname}'":
-    command => "UPDATE pg_group SET grolist = '${groupmembers}' WHERE groname = '${groupname}'",
-    unless => "SELECT 1 FROM pg_group WHERE groname = '${groupname}' AND grolist = '${groupmembers}'",
+  postgresql_psql {"${title}: UPDATE pg_group SET grolist = ARRAY${groupmembers} WHERE groname = '${groupname}'":
+    command => "UPDATE pg_group SET grolist = ARRAY${groupmembers} WHERE groname = '${groupname}'",
+    unless => "SELECT 1 FROM pg_group WHERE groname = '${groupname}' AND grolist = ARRAY${groupmembers}",
   }
 }
---
+++ spec/unit/defines/server/dbgroup_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+
+describe 'postgresql::server::dbgroup', :type => :define do
+
+  let :facts do
+    {
+      :osfamily => 'Debian',
+      :operatingsystem => 'Debian',
+      :operatingsystemrelease => '6.0',
+      :kernel => 'Linux',
+      :concat_basedir => tmpfilename('contrib'),
+      :id => 'root',
+      :path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    }
+  end
+
+  let :title do
+    'test'
+  end
+
+  context 'no members' do
+
+    let :pre_condition do
+     "class {'postgresql::server': dialect => 'postgres'}"
+    end
+  
+    it { is_expected.to contain_postgresql__server__dbgroup('test') }
+    it 'should have create group for test' do
+      is_expected.to contain_postgresql_psql('test: CREATE GROUP test').with({
+        'command'     => "CREATE GROUP 'test'",
+        'environment' => [],
+        'unless'      => "SELECT 1 FROM pg_group WHERE groname = 'test'",
+        'port'        => "5432",
+      })
+    end
+    it 'should have update pg_group for test group with groupmembers as []' do
+      is_expected.to contain_postgresql_psql("test: UPDATE pg_group SET grolist = [] WHERE groname = 'test'").with({
+        'command'     => "UPDATE pg_group SET grolist = [] WHERE groname = 'test'",
+        'environment' => [],
+        'unless'      => "SELECT 1 FROM pg_group WHERE groname = 'test' AND grolist = []",
+        'port'        => "5432",
+      })
+    end
+  end
+
+  context 'group containing test members' do
+
+    let :pre_condition do
+     "class {'postgresql::server': dialect => 'postgres'}"
+    end
+  
+    let :params do
+      {
+        :groupmembers => ['testuser1', 'testuser2'],
+      }
+    end
+
+    it { is_expected.to contain_postgresql__server__dbgroup('test') }
+    it 'should have create group for test' do
+      is_expected.to contain_postgresql_psql('test: CREATE GROUP test').with({
+        'command'     => "CREATE GROUP 'test'",
+        'environment' => [],
+        'unless'      => "SELECT 1 FROM pg_group WHERE groname = 'test'",
+        'port'        => "5432",
+      })
+    end
+    it 'should have update pg_group for test group with provided groupmembers' do
+      is_expected.to contain_postgresql_psql("test: UPDATE pg_group SET grolist = ARRAY[testuser1, testuser2] WHERE groname = 'test'").with({
+        'command'     => "UPDATE pg_group SET grolist = ARRAY[testuser1, testuser2] WHERE groname = 'test'",
+        'environment' => [],
+        'unless'      => "SELECT 1 FROM pg_group WHERE groname = 'test' AND grolist = ARRAY[testuser1, testuser2]",
+        'port'        => "5432",
+      })
+    end
+  end
+end
