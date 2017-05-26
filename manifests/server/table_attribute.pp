14567279ab2b818b3efaf10eb00be6f694b73317
Nick Walker <nick.walker@puppetlabs.com>
2017-05-26 10:53:31 -0700

    Implement absent support for table_atrribute
    Prior to this commit, you could only ensure the presence of table
    attributes.
    
    After this commit, you can supply ensure => 'absent' and the
    attribute will be RESET.

--- manifests/server/table_attribute.pp
+++ manifests/server/table_attribute.pp
@@ -1,25 +1,37 @@
 define postgresql::server::table_attribute (
+  Enum['absent','present'] $ensure = 'present',
   String  $db,
   String  $table,
   String  $attribute,
   String  $value,
-  String  $psql_user         = $postgresql::server::user,
-  String  $psql_group        = $postgresql::server::group,
-  Integer $port              = $postgresql::server::port,
-  Hash    $connect_settings  = $postgresql::server::default_connect_settings,
-  String  $psql_path         = $postgresql::server::psql_path
+  String  $psql_user        = $postgresql::server::user,
+  String  $psql_group       = $postgresql::server::group,
+  Integer $port             = $postgresql::server::port,
+  Hash    $connect_settings = $postgresql::server::default_connect_settings,
+  String  $psql_path        = $postgresql::server::psql_path
 ) {
 
-  $command = "ALTER TABLE ${table} SET ( ${attribute} = ${value} )"
+  Postgresql_psql {
+    db               => $db,
+    psql_user        => $psql_user,
+    psql_group       => $psql_group,
+    port             => $port,
+    connect_settings => $connect_settings,
+    psql_path        => $psql_path,
+  }
 
-  postgresql_psql { "${db}: ${command}" :
-      command          => $command,
-      unless           => "SELECT 1 FROM pg_class WHERE relname = '${table}' AND '${attribute}=${value}' = ANY (reloptions)",
-      db               => $db,
-      psql_user        => $psql_user,
-      psql_group       => $psql_group,
-      port             => $port,
-      connect_settings => $connect_settings,
-      psql_path        => $psql_path,
+  $create_command = "ALTER TABLE ${table} SET ( ${attribute} = ${value} )"
+  $reset_command = "ALTER TABLE ${table} RESET ( ${attribute} )"
+
+  if $ensure == 'present' {
+    postgresql_psql { "${db}: ${create_command}" :
+      command => $create_command,
+      unless  => "SELECT 1 FROM pg_class WHERE relname = '${table}' AND '${attribute}=${value}' = ANY (reloptions)",
     }
+  } else {
+    postgresql_psql { "${db}: ${reset_command}" :
+      command => $reset_command,
+      onlyif  => "SELECT 1 FROM pg_class WHERE relname = '${table}' AND CAST(reloptions as text) LIKE '%{attribute}%'",
+    }
+  }
 }
