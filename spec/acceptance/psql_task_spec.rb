8b8f4d56102d60bb1afef39a5d0f3689bfd4e44e
Paul Bailey <paulba@stedwards.edu>
2017-10-12 11:15:37 -0700

    add psql task

---
+++ spec/acceptance/psql_task_spec.rb
@@ -0,0 +1,23 @@
+# run a test task
+require 'spec_helper_acceptance'
+
+describe 'postgresql tasks' do
+  describe 'execute some sql' do
+    pp = <<-EOS
+        class { 'postgresql::server': } ->
+        postgresql::server::db { 'spec1':
+          user     => 'root1',
+          password => postgresql_password('root1', 'password'),
+        }
+    EOS
+
+    it 'sets up a postgresql instance' do
+      apply_manifest(pp, catch_failures: true)
+    end
+
+    it 'execute arbitary psql' do
+      result = run_task(task_name: 'postgresql::psql', params: 'sql="SELECT table_name FROM information_schema.tables WHERE table_schema = \'information_schema\';" host=localhost user=root1 password=password')
+      expect_multiple_regexes(result: result, regexes: [%r{columns}, %r{tables}, %r{Job completed. 1/1 nodes succeeded}])
+    end
+  end
+end
