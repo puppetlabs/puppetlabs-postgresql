34a8f61a6e7a9cd076b896b8cb3da3b7059c1767
MartyEwings <martyewings@gmail.com>
2017-11-23 00:36:13 +0000

    Update config_entry_spec.rb
    Allow default module PostgreSQL.conf location

--- spec/acceptance/server/config_entry_spec.rb
+++ spec/acceptance/server/config_entry_spec.rb
@@ -4,7 +4,6 @@ describe 'postgresql::server::config_entry' do
 
   let(:pp_setup) { <<-EOS
     class { 'postgresql::server':
-      postgresql_conf_path => '/tmp/postgresql.conf',
       }
     EOS
   }
@@ -25,7 +24,7 @@ describe 'postgresql::server::config_entry' do
     if version >= '9.3'
       it 'is expected to run idempotently' do
         apply_manifest(pp_test, :catch_failures => true)
-        apply_manifest(pp_test, :catch_changes => false)
+        apply_manifest(pp_test, :catch_changes => true)
       end
 
       it 'is expected to contain directories' do
