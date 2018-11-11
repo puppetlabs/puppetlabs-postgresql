363ba9f063073cee69393dd209ab648663f6151f
tphoney <tp@puppet.com>
2018-11-10 17:47:41 -0800

    Use latest solid_waffle standards

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ end
 ruby_version_segments = Gem::Version.new(RUBY_VERSION.dup).segments
 minor_version = ruby_version_segments[0..1].join('.')
 gem 'solid_waffle', path: '/Users/tp/workspace/git/solid-waffle'
+gem 'pdk', git: 'https://github.com/tphoney/pdk.git', branch: 'pin_cri'
 group :development do
   gem "fast_gettext", '1.1.0',                         require: false if Gem::Version.new(RUBY_VERSION.dup) < Gem::Version.new('2.1.0')
   gem "fast_gettext",                                  require: false if Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new('2.1.0')
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -9,34 +9,40 @@ if ENV['TARGET_HOST'].nil?
 else
   puts "TARGET_HOST #{ENV['TARGET_HOST']}"
   # load inventory
-  inventory_hash = load_inventory_hash
-  if host_in_group(inventory_hash, ENV['TARGET_HOST'], 'ssh_nodes')
+  inventory_hash = inventory_hash_from_inventory_file
+  node_config = config_from_node(inventory_hash, ENV['TARGET_HOST'])
+
+  if target_in_group(inventory_hash, ENV['TARGET_HOST'], 'ssh_nodes')
     set :backend, :ssh
     options = Net::SSH::Config.for(host)
-    options[:user] = 'root'
-    host = ENV['TARGET_HOST']
+    options[:user] = node_config.dig('ssh', 'user') unless node_config.dig('ssh', 'user').nil?
+    options[:port] = node_config.dig('ssh', 'port') unless node_config.dig('ssh', 'port').nil?
+    options[:password] = node_config.dig('ssh', 'password') unless node_config.dig('ssh', 'password').nil?
+    host = if ENV['TARGET_HOST'].include?(':')
+             ENV['TARGET_HOST'].split(':').first
+           else
+             ENV['TARGET_HOST']
+           end
     set :host,        options[:host_name] || host
     set :ssh_options, options
-  elsif host_in_group(inventory_hash, ENV['TARGET_HOST'], 'win_rm_nodes')
+  elsif target_in_group(inventory_hash, ENV['TARGET_HOST'], 'winrm_nodes')
     require 'winrm'
 
     set :backend, :winrm
-    set :os, :family => 'windows'
-    user = 'Administrator'
-    pass = 'Qu@lity!'
+    set :os, family: 'windows'
+    user = node_config.dig('winrm', 'user') unless node_config.dig('winrm', 'user').nil?
+    pass = node_config.dig('winrm', 'password') unless node_config.dig('winrm', 'password').nil?
     endpoint = "http://#{ENV['TARGET_HOST']}:5985/wsman"
 
     opts = {
-       user: user,
-       password: pass,
-       endpoint: endpoint,
-       operation_timeout: 300,
+      user: user,
+      password: pass,
+      endpoint: endpoint,
+      operation_timeout: 300,
     }
 
-    winrm = WinRM::Connection.new ( opts)
+    winrm = WinRM::Connection.new opts
     Specinfra.configuration.winrm = winrm
-  else
-    raise "#{ENV['TARGET_HOST']} is not a member of any handled groups, check inventory.yaml"
   end
 end
 
@@ -68,9 +74,9 @@ end
 
 def psql(psql_cmd, user = 'postgres', exit_codes = [0, 1], &block)
   psql = "psql #{psql_cmd}" 
-inventory_hash = load_inventory_hash
-targets = find_targets(nil, inventory_hash)
-host = targets.first.to_s
+  inventory_hash = inventory_hash_from_inventory_file
+  targets = find_targets(inventory_hash, nil)
+  host = targets.first.to_s
   run_command("su #{shellescape(user)} -c #{shellescape(psql)}", host, config: nil, inventory: inventory_hash)  
 #  shell("su #{shellescape(user)} -c #{shellescape(psql)}", acceptable_exit_codes: exit_codes, &block)
 end
