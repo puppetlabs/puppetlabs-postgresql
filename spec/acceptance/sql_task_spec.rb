09427fdf63f1f7061d5cca05f3754d014b68360b
tphoney <tp@puppet.com>
2018-11-30 13:33:42 +0000

    sql task test now works with swaffle

--- spec/acceptance/sql_task_spec.rb
+++ spec/acceptance/sql_task_spec.rb
@@ -1,24 +1,24 @@
 # run a test task
 require 'spec_helper_acceptance'
 
-describe 'postgresql task', if: '6.0.3' =~ %r{(5\.\d\.\d)} && !pe_install? do
-  describe 'sql task' do
-    pp = <<-MANIFEST
-        class { 'postgresql::server': } ->
-        postgresql::server::db { 'spec1':
-          user     => 'root1',
-          password => postgresql_password('root1', 'password'),
-        }
-    MANIFEST
+describe 'postgresql::sql task' do
+  let(:pp) do
+  <<-MANIFEST
+      class { 'postgresql::server': } ->
+      postgresql::server::db { 'spec1':
+        user     => 'root1',
+        password => postgresql_password('root1', 'password'),
+      }
+  MANIFEST
+end
 
-    it 'sets up a postgres db' do
-      apply_manifest(pp, catch_failures: true)
-    end
-
-    it 'execute some sql' do
-      # equates to 'psql -c "SELECT table_name FROM information_schema.tables WHERE table_schema = 'information_schema';"  --password --host localhost --dbname=spec1 --username root1'
-      result = run_task(task_name: 'postgresql::sql', params: 'sql="SELECT count(table_name) FROM information_schema.tables;" host=localhost user=root1 password=password user=root1 database=spec1')
-      expect_multiple_regexes(result: result, regexes: [%r{count}, %r{1 row}, %r{Job completed. 1/1 nodes succeeded|Ran on 1 node}])
-    end
+  it 'sets up a postgres db' do
+    apply_manifest(pp, catch_failures: true)
+  end
+  it 'execute the sql task' do
+    # equates to 'psql -c "SELECT table_name FROM information_schema.tables WHERE table_schema = 'information_schema';"  --password --host localhost --dbname=spec1 --username root1'
+    result = task_run('postgresql::sql', {'sql'=>'SELECT count(table_name) FROM information_schema.tables;', 'host'=>'localhost', 'user'=>'root1', 'password'=>'password', 'database'=>'spec1'})
+    expect(result.first["result"].to_s).to match %r{count}
+    expect(result.first['result'].to_s).to match %r{1 row}
   end
 end
