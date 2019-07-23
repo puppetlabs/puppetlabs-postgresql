da80f3569f156f1aea14e71269028671fba35a9e
Nick Walker <nick.walker@puppetlabs.com>
2019-07-23 08:53:18 -0700

    (MAINT) Make default function compatible with puppet 5

--- functions/default.pp
+++ functions/default.pp
@@ -5,6 +5,6 @@ function postgresql::default(
 
   #search for the variable name in params first
   #then fall back to globals if not found
-  getvar("postgresql::params::${parameter_name}",
+  pick( getvar("postgresql::params::${parameter_name}"),
     "postgresql::globals::${parameter_name}")
 }
