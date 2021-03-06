puppetlabs/puppetlabs-postgresql

commit 841187b0ec29726c0fe39ea2ad710a5ead42d2e0

Fix quotes to avoid shell injection

lib/puppet/provider/postgresql_psql/ruby.rb
@@ -16,7 +16,7 @@ Puppet::Type.type(:postgresql_psql).provide(:ruby) do
     command = [resource[:psql_path]]
     command.push('-d', resource[:db]) if resource[:db]
     command.push('-p', resource[:port]) if resource[:port]
-    command.push('-t', '-X', '-c', '"' + sql.gsub('"', '\"') + '"')
+    command.push('-t', '-X', '-c', sql)
 
     environment = fetch_environment
 
@@ -57,7 +57,6 @@ Puppet::Type.type(:postgresql_psql).provide(:ruby) do
   end
 
   def run_command(command, user, group, environment)
-    command = command.join ' '
     output = Puppet::Util::Execution.execute(command, uid: user,
                                                       gid: group,
                                                       failonfail: false,

commit 5b14a1208c45a9913d36d1fc1d69d15c59fa2f9a

Ensure port is a string in psql command
In 841187b0ec29726c0fe39ea2ad710a5ead42d2e0 a shell execution
vulnerability was fixed by passing an array. In my environment (Debian
10, Ruby 2.5, Puppet 5) it was failing because port was an Integer and
command execution only allows Strings. This explicitly converts the port
to a string.

Fixes: 841187b0ec29726c0fe39ea2ad710a5ead42d2e0

lib/puppet/provider/postgresql_psql/ruby.rb
@@ -15,7 +15,7 @@ Puppet::Type.type(:postgresql_psql).provide(:ruby) do
 
     command = [resource[:psql_path]]
     command.push('-d', resource[:db]) if resource[:db]
-    command.push('-p', resource[:port]) if resource[:port]
+    command.push('-p', resource[:port].to_s) if resource[:port]
     command.push('-t', '-X', '-c', sql)
 
     environment = fetch_environment