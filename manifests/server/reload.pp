puppetlabs/puppetlabs-postgresql

commit ef1681f5e50ae7976484dd1daf6a18a92bcb9191

Changes to support OpenBSD.
Added $service_reload parameter to params.pp, in order to allow
a reload of the postgresql server on OpenBSD, which apparently doesn't
have a service binary.

manifests/server/reload.pp
@@ -2,10 +2,11 @@
 class postgresql::server::reload {
   $service_name   = $postgresql::server::service_name
   $service_status = $postgresql::server::service_status
+  $service_reload = $postgresql::server::service_reload
 
   exec { 'postgresql_reload':
     path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-    command     => "service ${service_name} reload",
+    command     => $service_reload,
     onlyif      => $service_status,
     refreshonly => true,
     require     => Class['postgresql::server::service'],

manifests/globals.pp
@@ -85,6 +85,7 @@ class postgresql::globals (
       default => '9.2',
     },
     'FreeBSD' => '93',
+    'OpenBSD' => '9.3',
     'Suse' => $::operatingsystem ? {
       'SLES' => '91',
       default => undef,

manifests/params.pp
@@ -60,6 +60,7 @@ class postgresql::params inherits postgresql::globals {
       $psql_path            = pick($psql_path, "${bindir}/psql")
 
       $service_status      = $service_status
+      $service_reload      = "service ${service_name} reload"
       $perl_package_name   = pick($perl_package_name, 'perl-DBD-Pg')
       $python_package_name = pick($python_package_name, 'python-psycopg2')
 
@@ -98,6 +99,7 @@ class postgresql::params inherits postgresql::globals {
       $psql_path            = pick($psql_path, "${bindir}/psql")
 
       $service_status      = $service_status
+      $service_reload      = "service ${service_name} reload"
       $python_package_name = pick($python_package_name, 'python-psycopg2')
       # Archlinux does not have a perl::DBD::Pg package
       $perl_package_name = pick($perl_package_name, 'undef')
@@ -141,6 +143,7 @@ class postgresql::params inherits postgresql::globals {
       $datadir              = pick($datadir, "/var/lib/postgresql/${version}/main")
       $confdir              = pick($confdir, "/etc/postgresql/${version}/main")
       $service_status       = pick($service_status, "/etc/init.d/${service_name} status | /bin/egrep -q 'Running clusters: .+|online'")
+      $service_reload       = "service ${service_name} reload"
       $psql_path            = pick($psql_path, '/usr/bin/psql')
     }
 
@@ -163,6 +166,31 @@ class postgresql::params inherits postgresql::globals {
       $datadir              = pick($datadir, '/usr/local/pgsql/data')
       $confdir              = pick($confdir, $datadir)
       $service_status       = pick($service_status, "/usr/local/etc/rc.d/${service_name} onestatus")
+      $service_reload       = "service ${service_name} reload"
+      $psql_path            = pick($psql_path, "${bindir}/psql")
+
+      $needs_initdb         = pick($needs_initdb, true)
+    }
+
+    'OpenBSD': {
+      $user                = pick($user, '_postgresql')
+      $group               = pick($group, '_postgresql')
+
+      $client_package_name  = pick($client_package_name, 'postgresql-client')
+      $server_package_name  = pick($server_package_name, 'postgresql-server')
+      $contrib_package_name = pick($contrib_package_name, 'postgresql-contrib')
+      $devel_package_name   = pick($devel_package_name, 'postgresql-client')
+      $java_package_name    = pick($java_package_name, 'postgresql-jdbc')
+      $perl_package_name    = pick($perl_package_name, 'databases/p5-DBD-Pg')
+      $plperl_package_name  = undef
+      $python_package_name  = pick($python_package_name, 'py-psycopg2')
+
+      $service_name         = pick($service_name, 'postgresql')
+      $bindir               = pick($bindir, '/usr/local/bin')
+      $datadir              = pick($datadir, '/var/postgresql/data')
+      $confdir              = pick($confdir, $datadir)
+      $service_status       = pick($service_status, "/etc/rc.d/${service_name} check")
+      $service_reload       = "/etc/rc.d/${service_name} reload"
       $psql_path            = pick($psql_path, "${bindir}/psql")
 
       $needs_initdb         = pick($needs_initdb, true)
@@ -187,6 +215,7 @@ class postgresql::params inherits postgresql::globals {
       $datadir              = pick($datadir, '/var/lib/pgsql/data')
       $confdir              = pick($confdir, $datadir)
       $service_status       = pick($service_status, "/etc/init.d/${service_name} status")
+      $service_reload       = "/etc/init.d/${service_name} reload"
       $psql_path            = pick($psql_path, "${bindir}/psql")
 
       $needs_initdb         = pick($needs_initdb, true)

manifests/server.pp
@@ -13,6 +13,7 @@ class postgresql::server (
   $service_manage             = $postgresql::params::service_manage,
   $service_name               = $postgresql::params::service_name,
   $service_provider           = $postgresql::params::service_provider,
+  $service_reload             = $postgresql::params::service_reload,
   $service_status             = $postgresql::params::service_status,
   $default_database           = $postgresql::params::default_database,
 

commit 06de19b4e9ab0b1694243ed5501d30baf7625dc6

Remove unused variable in reload.pp
When service_reload was introduced, it made service_name redundant.

Fixes: ef1681f5e50ae7976484dd1daf6a18a92bcb9191

manifests/server/reload.pp
@@ -1,6 +1,5 @@
 # @api private
 class postgresql::server::reload {
-  $service_name   = $postgresql::server::service_name
   $service_status = $postgresql::server::service_status
   $service_reload = $postgresql::server::service_reload
 