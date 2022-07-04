puppetlabs/puppetlabs-postgresql

commit 7b282bdd92a17495f6b192f946271e1e18b0c071

(MODULES-9219) - puppetlabs-postgresql : catalog compilation fails when the service command is not installed
The service command is deprecated and is not installed by default on
modern Redhat releases.  Updated class parameters to define the service
status and service reload commands based on what OS is installed on the node.

manifests/params.pp
@@ -78,10 +78,33 @@ class postgresql::params inherits postgresql::globals {
         }
         $confdir                = pick($confdir, $datadir)
       }
+
+      case $::operatingsystem {
+        'Amazon': {
+          $service_reload = "service ${service_name} reload"
+          $service_status = "service ${service_name} status"
+        }
+
+        # RHEL 5 uses SysV init, RHEL 6 uses upstart.  RHEL 7 and 8 both use systemd.
+        'RedHat', 'CentOS', 'Scientific', 'OracleLinux': {
+          if $::operatingsystemrelease =~ /^[78].*/ {
+            $service_reload = "systemctl reload ${service_name}"
+            $service_status = "systemctl status ${service_name}"
+          } else {
+            $service_reload = "service ${service_name} reload"
+            $service_status = "service ${service_name} status"
+          }
+        }
+
+        # Default will catch Fedora which uses systemd
+        default: {
+          $service_reload = "systemctl reload ${service_name}"
+          $service_status = "systemctl status ${service_name}"
+        }
+      }
+
       $psql_path           = pick($psql_path, "${bindir}/psql")
 
-      $service_status      = $service_status
-      $service_reload      = "service ${service_name} reload"
       $perl_package_name   = pick($perl_package_name, 'perl-DBD-Pg')
       $python_package_name = pick($python_package_name, 'python-psycopg2')
 

commit 4b4000a00046afc50a019d61790b8494383af29e

Respect $service_status on Red Hat-based distros
Fixes: 7b282bdd92a17495f6b192f946271e1e18b0c071

manifests/params.pp
@@ -84,10 +84,10 @@ class postgresql::params inherits postgresql::globals {
 
       if pick($service_provider, $facts['service_provider']) == 'systemd' {
         $service_reload = "systemctl reload ${service_name}"
-        $service_status = "systemctl status ${service_name}"
+        $service_status = pick($service_status, "systemctl status ${service_name}")
       } else {
         $service_reload = "service ${service_name} reload"
-        $service_status = "service ${service_name} status"
+        $service_status = pick($service_status, "service ${service_name} status")
       }
 
       $psql_path           = pick($psql_path, "${bindir}/psql")