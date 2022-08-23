puppetlabs/puppetlabs-postgresql

commit 37e5bea4399ab926e6041190347e24d55a08a51c

(MODULES-11251) Add support for backup provider "pg_dump"

manifests/server.pp
@@ -77,6 +77,10 @@
 # @param roles Specifies a hash from which to generate postgresql::server::role resources.
 # @param config_entries Specifies a hash from which to generate postgresql::server::config_entry resources.
 # @param pg_hba_rules Specifies a hash from which to generate postgresql::server::pg_hba_rule resources.
+
+# @param backup_enable Whether a backup job should be enabled.
+# @param backup_options A hash of options that should be passed through to the backup provider.
+# @param backup_provider Specifies the backup provider to use.
 #
 # @param version Deprecated. Use postgresql::globals instead. Sets PostgreSQL version
 #
@@ -152,6 +156,10 @@ class postgresql::server (
   Hash[String, Any] $config_entries = {},
   Hash[String, Hash] $pg_hba_rules  = {},
 
+  Boolean $backup_enable = $postgresql::params::backup_enable,
+  Hash $backup_options = {},
+  Enum['pg_dump'] $backup_provider = $postgresql::params::backup_provider,
+
   #Deprecated
   $version                    = undef,
 ) inherits postgresql::params {
@@ -199,6 +207,19 @@ class postgresql::server (
   $pg_hba_rules.each |$rule_name, $rule| {
     postgresql::server::pg_hba_rule { $rule_name:
       * => $rule,
+    }
+  }
+
+  if $backup_enable {
+    case $backup_provider {
+      'pg_dump': {
+        class { 'postgresql::backup::pg_dump':
+          * => $backup_options,
+        }
+      }
+      default: {
+        fail("Unsupported backup provider '${backup_provider}'.")
+      }
     }
   }
 }

.fixtures.yml
@@ -5,6 +5,7 @@ fixtures:
       repo: "https://github.com/puppetlabs/puppetlabs-augeas_core.git"
       puppet_version: ">= 6.0.0"
     concat: "https://github.com/puppetlabs/puppetlabs-concat.git"
+    cron_core: "https://github.com/puppetlabs/puppetlabs-cron_core.git"
     facts: 'https://github.com/puppetlabs/puppetlabs-facts.git'
     firewall: "https://github.com/puppetlabs/puppetlabs-firewall.git"
     provision: "https://github.com/puppetlabs/provision.git"

manifests/backup/pg_dump.pp
@@ -1,0 +1,151 @@
+# @summary
+#   "Provider" for pg_dump backup
+#
+# @api private
+#
+# @param compress
+#   Whether or not to compress the backup. Support for compression also depends on other backup parameters.
+# @param databases
+#   Databases to backup. By default `[]` will back up all databases.
+# @param db_user
+#   PostgreSQL user to create with superuser privileges.
+# @param db_password
+#   Password to create for `$db_user`.
+# @param dir
+#   Directory to store backup.
+# @param dir_mode
+#   Permissions applied to the backup directory. This parameter is passed directly to the file resource.
+# @param dir_owner
+#   Owner for the backup directory. This parameter is passed directly to the file resource.
+# @param dir_group
+#   Group owner for the backup directory. This parameter is passed directly to the file resource.
+# @param format
+#   Backup format to use, must be supported by pg_dump or pg_dumpall. The choice will affect other options, i.e. compression.
+# @param install_cron
+#   Manage installation of cron package.
+# @param manage_user
+#   Manage creation of the backup user.
+# @param optional_args
+#   Specifies an array of optional arguments which should be passed through to the backup tool. These options are not validated, unsupported options may break the backup.
+# @param postscript
+#   One or more scripts that are executed when the backup is finished. This could be used to sync the backup to a central store.
+# @param prescript
+#   One or more scripts that are executed before the backup begins.
+# @param rotate
+#   Backup rotation interval in 24 hour periods.
+# @param success_file_path
+#   Specify a path where upon successful backup a file should be created for checking purposes.
+# @param time
+#   An array of two elements to set the backup time. Allows `['23', '5']` (i.e., 23:05) or `['3', '45']` (i.e., 03:45) for HH:MM times.
+# @param weekday
+#   Weekdays on which the backup job should run. Defaults to `*`. This parameter is passed directly to the cron resource.
+#
+class postgresql::backup::pg_dump (
+  Boolean $compress = true,
+  Array $databases = [],
+  Boolean $delete_before_dump = false,
+  String[1] $dir,
+  String[1] $dir_group = '0',
+  String[1] $dir_mode = '0700',
+  String[1] $dir_owner = 'root',
+  Enum['present','absent'] $ensure = 'present',
+  Enum['plain','custom','directory','tar'] $format = 'plain',
+  Boolean $install_cron = true,
+  Boolean $manage_user = false,
+  Array $optional_args = [],
+  Stdlib::Absolutepath $pgpass_path = '/root/.pgpass',
+  Integer $rotate = 30,
+  Stdlib::Absolutepath $script_path = '/usr/local/sbin/pg_dump.sh',
+  Stdlib::Absolutepath $success_file_path = '/tmp/pgbackup_success',
+  String[1] $template = 'postgresql/pg_dump.sh.epp',
+  Array $time = ['23', '5'],
+  String[1] $weekday = '*',
+  Optional[Variant[String, Sensitive[String]]] $db_password = undef,
+  Optional[String[1]] $db_user = undef,
+  Optional[String[1]] $package_name = undef,
+  Optional[String[1]] $post_script = undef,
+  Optional[String[1]] $pre_script = undef,
+) {
+  # Install required packages
+  if $package_name {
+    ensure_packages($package_name)
+  }
+  if $install_cron {
+    if $facts['os']['family'] == 'RedHat' {
+      ensure_packages('cronie')
+    } elsif $facts['os']['family'] != 'FreeBSD' {
+      ensure_packages('cron')
+    }
+  }
+
+  # Setup db user with required permissions
+  if $manage_user and $db_user and $db_password {
+    # Create user with superuser privileges
+    postgresql::server::role { $db_user:
+      ensure        => $ensure,
+      password_hash => postgresql::postgresql_password($db_user, $db_password),
+      superuser     => true,
+    }
+
+    # Allow authentication from localhost
+    postgresql::server::pg_hba_rule { 'local access as backup user':
+      type        => 'local',
+      database    => 'all',
+      user        => $db_user,
+      auth_method => 'md5',
+      order       => 1,
+    }
+  }
+
+  # Create backup directory
+  file { $dir:
+    ensure => 'directory',
+    mode   => $dir_mode,
+    owner  => $dir_owner,
+    group  => $dir_group,
+  }
+
+  # Create backup script
+  file { $script_path:
+    ensure  => $ensure,
+    mode    => '0700',
+    owner   => 'root',
+    group   => '0', # Use GID for compat with Linux and BSD.
+    content => epp($template, {
+      compress           => $compress,
+      databases          => $databases,
+      db_user            => $db_user,
+      delete_before_dump => $delete_before_dump,
+      dir                => $dir,
+      format             => $format,
+      optional_args      => $optional_args,
+      post_script        => $post_script,
+      pre_script         => $pre_script,
+      rotate             => $rotate,
+      success_file_path  => $success_file_path,
+    }),
+  }
+
+  # Create password file for pg_dump
+  file { $pgpass_path:
+    ensure    => $ensure,
+    mode      => '0600',
+    owner     => 'root',
+    group     => '0', # Use GID for compat with Linux and BSD.
+    content   => inline_epp('*:*:*:<%= $db_user %>:<%= $db_password %>',{
+      db_password => $db_password,
+      db_user     => $db_user,
+    }),
+    show_diff => false,
+  }
+
+  # Create cron job
+  cron { 'pg_dump backup job':
+    ensure  => $ensure,
+    command => $script_path,
+    user    => 'root',
+    hour    => $time[0],
+    minute  => $time[1],
+    weekday => $weekday,
+  }
+}

manifests/params.pp
@@ -31,6 +31,9 @@ class postgresql::params inherits postgresql::globals {
   $manage_logdir                = true
   $manage_xlogdir               = true
 
+  $backup_enable = false
+  $backup_provider = 'pg_dump'
+
   # Amazon Linux's OS Family is 'Linux', operating system 'Amazon'.
   case $facts['os']['family'] {
     'RedHat', 'Linux': {

spec/classes/server_spec.rb
@@ -256,4 +256,62 @@ describe 'postgresql::server' do
         .with_address('192.0.2.100')
     end
   end
+
+  describe 'backup_enable => false' do
+    let(:params) { { backup_enable: false } }
+
+    it { is_expected.to contain_class('postgresql::server') }
+    it { is_expected.not_to contain_class('postgresql::backup::pg_dump') }
+    it { is_expected.not_to contain_file('/root/.pgpass') }
+    it { is_expected.not_to contain_file('/usr/local/sbin/pg_dump.sh') }
+    it { is_expected.not_to contain_cron('pg_dump backup job') }
+  end
+
+  describe 'backup_enable => true' do
+    let(:params) do
+      {
+        backup_enable: true,
+        backup_provider: 'pg_dump',
+        backup_options: {
+          db_user: 'backupuser',
+          db_password: 'backuppass',
+          dir: '/tmp/backuptest',
+          manage_user: true,
+        },
+      }
+    end
+
+    it { is_expected.to contain_class('postgresql::server') }
+    it { is_expected.to contain_class('postgresql::backup::pg_dump') }
+    it {
+      is_expected.to contain_postgresql__server__role('backupuser')
+        .with_superuser(true)
+    }
+    it {
+      is_expected.to contain_postgresql__server__pg_hba_rule('local access as backup user')
+        .with_type('local')
+        .with_database('all')
+        .with_user('backupuser')
+        .with_auth_method('md5')
+    }
+    it {
+      is_expected.to contain_file('/root/.pgpass')
+        .with_content(%r{.*:backupuser:.*})
+    }
+    it {
+      is_expected.to contain_file('/usr/local/sbin/pg_dump.sh')
+        .with_content(%r{.*pg_dumpall \$_pg_args --file=\$\{FILE\} \$@.*})
+    }
+    it {
+      is_expected.to contain_cron('pg_dump backup job')
+        .with(
+          ensure: 'present',
+          command: '/usr/local/sbin/pg_dump.sh',
+          user: 'root',
+          hour: '23',
+          minute: '5',
+          weekday: '*',
+        )
+    }
+  end
 end

templates/pg_dump.sh.epp
@@ -1,0 +1,103 @@
+<%- if $facts['kernel'] == 'Linux' { -%>
+#!/bin/bash
+<%- } else { -%>
+#!/bin/sh
+<%- } -%>
+# This file is managed by Puppet. DO NOT EDIT.
+#
+# A wrapper for pg_dump
+
+# Backup config
+ROTATE=<%= $rotate %>
+BASEDIR="<%= $dir %>"
+DIR="${BASEDIR}/$(date +%F_%H-%M-%S)"
+
+# Pattern %FILENAME% will be replace or removed, depending
+# on the pg_dump parameters.
+TEMPLATE="${DIR}/%FILENAME%"
+
+# Use a filename suffix to better distinguish different file types.
+SUFFIX=".pgdump"
+
+# Ensure backup directory exist.
+mkdir -p $DIR
+
+<%- if $facts['kernel'] == 'Linux' { -%>
+set -o pipefail
+<%- } -%>
+
+<% if $pre_script { -%>
+  <%- flatten($pre_script).each |$_script| { %>
+<%= $_script %>
+  <%- } -%>
+<% } -%>
+
+cleanup()
+{
+  <%- if $facts['kernel'] == 'SunOS' { -%>
+    gfind "${BASEDIR}/" -mindepth 1 -maxdepth 1 -mtime +${ROTATE} -print0 | gxargs -0 -r rm -rf
+  <%- } else { -%>
+    find "${BASEDIR}/" -mindepth 1 -maxdepth 1 -mtime +${ROTATE} -print0 | xargs -0 -r rm -rf
+  <%- } -%>
+}
+
+<% if $delete_before_dump { -%>
+# Remove outdated backups unconditionally before making new backups.
+cleanup
+<% } -%>
+
+_pg_args=''
+
+<%- if $format == 'directory' { -%>
+# The 'directory' format expects a target directory instead of a file.
+TEMPLATE=$DIR
+<%- } -%>
+
+<%- if $db_user { -%>
+_pg_args="${_pg_args} --username=<%= $db_user %>"
+<%- } -%>
+
+<%- if $optional_args { -%>
+  <%- $optional_args.each |$_arg| { -%>
+_pg_args="${_pg_args} <%= $_arg %>"
+  <%- } -%>
+<%- } -%>
+
+<%- if $databases and $databases =~ Array and !empty($databases) { -%>
+_pg_args="${_pg_args} --format=<%= $format %>"
+
+<%# Compression is only supported by pg_dump, but not by pg_dumpall. -%>
+<%- if !$compress { -%>
+_pg_args="${_pg_args} --compress=0"
+<%# The tar archive format does not support compression. -%>
+<%- } elsif $format != 'tar' { -%>
+_pg_args="${_pg_args} --compress=9"
+SUFFIX="${SUFFIX}.gz"
+<%- } -%>
+
+# Dump only selected databases
+  <%- $databases.each |$_db| { -%>
+FILE=`echo $TEMPLATE | sed "s/%FILENAME%/<%= $_db %>$SUFFIX/;"`
+pg_dump $_pg_args --file=${FILE} $@ <%= $_db %>
+  <%- } -%>
+<%- } else { -%>
+# Dump the whole instance
+FILE=`echo $TEMPLATE | sed "s/%FILENAME%/all$SUFFIX/;"`
+pg_dumpall $_pg_args --file=${FILE} $@
+<%- } -%>
+
+<% unless $delete_before_dump { -%>
+# Remove outdated backups only if the new backup was successful.
+if [ $? -eq 0 ] ; then
+    cleanup
+    <%- if $success_file_path { -%>
+    touch <%= $success_file_path %>
+    <%- } -%>
+fi
+<% } -%>
+
+<% if $post_script { -%>
+  <%- flatten($post_script).each |$_script| { %>
+<%= $_script %>
+  <%- } -%>
+<% } -%>

commit 4f370fef14dffdeeb9a8161f385eaa9c1e994fbc

Fix puppet-strings documentation
In 37e5bea4399ab926e6041190347e24d55a08a51c an empty line was added,
splitting the documentation block. Removing the empty line creates a
single documentation block again.

Fixes: 37e5bea4399ab926e6041190347e24d55a08a51c

manifests/server.pp
@@ -77,7 +77,7 @@
 # @param roles Specifies a hash from which to generate postgresql::server::role resources.
 # @param config_entries Specifies a hash from which to generate postgresql::server::config_entry resources.
 # @param pg_hba_rules Specifies a hash from which to generate postgresql::server::pg_hba_rule resources.
-
+#
 # @param backup_enable Whether a backup job should be enabled.
 # @param backup_options A hash of options that should be passed through to the backup provider.
 # @param backup_provider Specifies the backup provider to use.