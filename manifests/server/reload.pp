44d565237917608da770a9114eee7678ed9afc40
Simon Hoenscheid <code@simonhoenscheid.de>
2023-01-27 18:39:35 +0100

    Add multi instance support, refactoring reload.pp (6/x)
    This commit adds changes a class to add multi instance support to this module.
    The general idea is to first copy all classes which are used and create defines from them.
    These classes will use the defines as is. Necessary changes for the instances itself will be added to the classes and defined types at a later point.
    This ensures, the module will work as it does right now and there are no breaking changes.

--- manifests/server/reload.pp
+++ manifests/server/reload.pp
@@ -1,13 +1,7 @@
 # @api private
 class postgresql::server::reload {
-  $service_status = $postgresql::server::service_status
-  $service_reload = $postgresql::server::service_reload
-
-  exec { 'postgresql_reload':
-    path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-    command     => $service_reload,
-    onlyif      => $service_status,
-    refreshonly => true,
-    require     => Class['postgresql::server::service'],
+  postgresql::server::instance_reload { 'main':
+    service_status => $postgresql::server::service_status,
+    service_reload => $postgresql::server::service_reload,
   }
 }
