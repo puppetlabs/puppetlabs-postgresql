a3de3258d2173184b64f4f7bb9696df025e99486
jordanbreen28 <jordan.breen@perforce.com>
2023-04-19 14:34:57 +0100

    (CONT-792) - Layout/HeredocIndentation

--- lib/puppet/type/postgresql_replication_slot.rb
+++ lib/puppet/type/postgresql_replication_slot.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Puppet::Type.newtype(:postgresql_replication_slot) do
-  @doc = <<-EOS
-@summary Manages Postgresql replication slots.
+  @doc = <<~EOS
+    @summary Manages Postgresql replication slots.
 
-This type allows to create and destroy replication slots
-to register warm standby replication on a Postgresql
-primary server.
+    This type allows to create and destroy replication slots
+    to register warm standby replication on a Postgresql
+    primary server.
   EOS
 
   ensurable
