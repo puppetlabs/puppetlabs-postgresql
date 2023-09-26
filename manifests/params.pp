922474cdd338e642d54756070261e84c32e6c254
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-09-26 15:38:12 +0200

    Drop code compatibility for non-systemd Red Hat
    Only EL6 used sysvinit, but that's no longer supported by the module.

--- manifests/params.pp
+++ manifests/params.pp
@@ -85,13 +85,8 @@ class postgresql::params inherits postgresql::globals {
         $postgresql_conf_mode   = pick($postgresql_conf_mode, '0600')
       }
 
-      if pick($service_provider, $facts['service_provider']) == 'systemd' {
-        $service_reload = "systemctl reload ${service_name}"
-        $service_status = pick($service_status, "systemctl status ${service_name}")
-      } else {
-        $service_reload = "service ${service_name} reload"
-        $service_status = pick($service_status, "service ${service_name} status")
-      }
+      $service_reload = "systemctl reload ${service_name}"
+      $service_status = pick($service_status, "systemctl status ${service_name}")
 
       $psql_path           = pick($psql_path, "${bindir}/psql")
 
