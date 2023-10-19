e981adb6b6c53eaa36a02ae6c5d1304e46ae7008
Tim Meusel <tim@bastelfreak.de>
2023-10-19 19:28:00 +0200

    (#1532) Replace ParserError with Puppet::Error
    I'm not sure how we ended up with ParserError in the
    provider. This exception doesn't exist in Ruby.
    Puppet ships ther own exception, Puppet::Error. It
    probably makes sense to raise that instead.
    
    Fixes 179472ba261a7d0847fdb93fdcb1d43741c4cdde
    
    Alternative implementation for https://github.com/puppetlabs/puppetlabs-postgresql/pull/1538

--- spec/unit/provider/postgresql_conf/ruby_spec.rb
+++ spec/unit/provider/postgresql_conf/ruby_spec.rb
@@ -4,13 +4,14 @@ require 'spec_helper'
 provider_class = Puppet::Type.type(:postgresql_conf).provider(:ruby)
 
 describe provider_class do
-  let(:resource) { Puppet::Type.type(:postgresql_conf).new(name: 'foo', value: 'bar') }
+  let(:resource) { Puppet::Type.type(:postgresql_conf).new(name: 'foo', key: 'foo', value: 'bar') }
   let(:provider) { resource.provider }
 
   before(:each) do
     allow(provider).to receive(:file_path).and_return('/tmp/foo')
     allow(provider).to receive(:read_file).and_return('foo = bar')
     allow(provider).to receive(:write_file).and_return(true)
+    allow(provider).to receive(:resource).and_return(key: 'your_key', line_number: 1, value: 'foo')
   end
   # rubocop:enable RSpec/ReceiveMessages
 
@@ -26,8 +27,27 @@ describe provider_class do
     expect(provider).to respond_to(:add_header)
   end
 
-  it 'has a method exists?' do
-    expect(provider).to respond_to(:exists?)
+  describe '#exists?' do
+    it 'returns true when a matching config item is found' do
+      config_data = [{ key: 'your_key', value: 'your_value' }]
+      expect(provider).to receive(:parse_config).and_return(config_data)
+
+      expect(provider.exists?).to be true
+    end
+
+    it 'returns false when no matching config item is found' do
+      config_data = [{ key: 'other_key', value: 'other_value' }]
+      expect(provider).to receive(:parse_config).and_return(config_data)
+
+      expect(provider.exists?).to be false
+    end
+
+    it 'raises an error when multiple matching config items are found' do
+      config_data = [{ key: 'your_key', value: 'value1' }, { key: 'your_key', value: 'value2' }]
+      expect(provider).to receive(:parse_config).and_return(config_data)
+
+      expect { provider.exists? }.to raise_error(Puppet::Error, 'found multiple config items of your_key, please fix this')
+    end
   end
 
   it 'has a method create' do
