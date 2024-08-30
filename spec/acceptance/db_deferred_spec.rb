2122477ab624f65478a544cde242b6c35c973ffc
Ben Morrice <ben.morrice@cern.ch>
2024-08-30 11:31:48 +0200

    Don't use type alias in `postgresql_password`
    Type aliases from modules aren't available on the agent.
    See https://puppet.atlassian.net/browse/PUP-7197
    
    This means that calls to `postgresql::postgresql_password` can't be
    deferred if the `hash` parameter uses the type
    `Postgresql::Pg_password_encryption`. Instead we have to use the raw
    `Enum`, (which unfortunately causes code duplication, but this is
    unavoidable).
    
    Using this function with `Deferred` is not just a theoretical use-case.
    The module itself tries to here.
    https://github.com/puppetlabs/puppetlabs-postgresql/blob/20704ffae24dcf970784697b1798c09d026fb7f8/manifests/server/role.pp#L162

---
+++ spec/acceptance/db_deferred_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'spec_helper_acceptance'
+
+describe 'postgresql::server::db:' do
+  let(:user) { 'user_test' }
+  let(:password) { 'deferred_password_test' }
+  let(:database) { 'test_database' }
+
+  let(:pp_one) do
+    <<~MANIFEST
+      $user = '#{user}'
+      $password = '#{password}'
+      $database = '#{database}'
+
+      include postgresql::server
+      postgresql::server::db { $database:
+         user     => $user,
+         password => Deferred('unwrap', [$password]),
+      }
+    MANIFEST
+  end
+
+  it 'creates a database with with the password in the deferred function' do
+    apply_manifest(pp_one)
+    psql_cmd = "PGPASSWORD=#{password} PGUSER=#{user} PGDATABASE=#{database} psql -h 127.0.0.1 -d postgres -c '\\q'"
+    run_shell("cd /tmp; su #{shellescape('postgres')} -c #{shellescape(psql_cmd)}",
+              acceptable_exit_codes: [0])
+  end
+end
