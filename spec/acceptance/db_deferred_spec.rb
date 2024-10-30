f4cc38a7c91939919a077da5d6e76a36e63f397d
Ben Morrice <ben.morrice@cern.ch>
2024-10-30 09:25:02 +0100

    Simplify Deferred postgresql::server::db Deferred test

--- spec/acceptance/db_deferred_spec.rb
+++ spec/acceptance/db_deferred_spec.rb
@@ -2,45 +2,32 @@
 
 require 'spec_helper_acceptance'
 
-describe 'postgresql::server::db' do
-  before(:all) do
-    LitmusHelper.instance.run_shell("cd /tmp; su 'postgres' -c 'pg_ctl stop -D /var/lib/pgsql/data/ -m fast'", acceptable_exit_codes: [0, 1]) unless os[:family].match?(%r{debian|ubuntu})
-  end
+describe 'postgresql::server::db:' do
+  let(:user) { 'user_test' }
+  let(:password) { 'deferred_password_test' }
+  let(:database) { 'test_database' }
 
-  it 'creates a database with a deferred password' do
-    tmpdir = run_shell('mktemp').stdout
-    pp = <<-MANIFEST
-      class { 'postgresql::server':
-        postgres_password => 'space password',
-      }
-      postgresql::server::tablespace { 'postgresql-test-db':
-        location => '#{tmpdir}',
-      } ->
-      postgresql::server::db { 'postgresql-test-db':
-        comment    => 'testcomment',
-        user       => 'test-user',
-        password   => Deferred('unwrap', ['test1'])
-        tablespace => 'postgresql-test-db',
+  let(:pp_one) do
+    <<-MANIFEST.unindent
+      $user = #{user}
+      $password = #{password}
+      $database = #{database}
+
+      include postgresql::server
+      postgresql::server::db { $database:
+         user     => $user,
+         password => Deferred('unwrap', [$password]),
       }
     MANIFEST
+  end
 
-    idempotent_apply(pp)
-
-    # Verify that the postgres password works
-    run_shell("echo 'localhost:*:*:postgres:'space password'' > /root/.pgpass")
-    run_shell('chmod 600 /root/.pgpass')
-    run_shell("psql -U postgres -h localhost --command='\\l'")
-
-    result = psql('--command="select datname from pg_database" "postgresql-test-db"')
-    expect(result.stdout).to match(%r{postgresql-test-db})
-    expect(result.stderr).to eq('')
-
-    result = psql('--command="SELECT 1 FROM pg_roles WHERE rolname=\'test-user\'"')
-    expect(result.stdout).to match(%r{\(1 row\)})
-
-    result = psql("--dbname postgresql-test-db --command=\"SELECT pg_catalog.shobj_description(d.oid, 'pg_database') FROM pg_catalog.pg_database d WHERE datname = 'postgresql-test-db' AND pg_catalog.shobj_description(d.oid, 'pg_database') = 'testcomment'\"") # rubocop:disable Layout/LineLength
-    expect(result.stdout).to match(%r{\(1 row\)})
-  ensure
-    psql('--command=\'drop database "postgresql-test-db"\'')
+  it 'creates a database with with the password in the deferred function' do
+    if run_shell('puppet --version').stdout[0].to_i < 7
+      skip # Deferred function fixes only in puppet 7, see https://tickets.puppetlabs.com/browse/PUP-11518
+    end
+    apply_manifest(pp_one)
+    psql_cmd = "PGPASSWORD=#{password} PGUSER=#{user} PGDATABASE=#{database} psql -h 127.0.0.1 -d postgres -c '\\q'"
+    run_shell("cd /tmp; su #{shellescape('postgres')} -c #{shellescape(psql_cmd)}",
+              acceptable_exit_codes: [0])
   end
 end
