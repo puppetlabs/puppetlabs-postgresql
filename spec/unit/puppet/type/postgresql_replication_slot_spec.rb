ae3d9d572a513dedbc93e7c84df7d0f8517ec5e3
Matt Darcy <matt@projecthugo.co.uk>
2026-08-05 11:12:43 +0100

    Fix postgresql_replication_slot type spec testing the wrong type
    The describe block named postgresql_replication_slot, but its subject
    instantiated Puppet::Type.type(:postgresql_psql) instead, so it never
    actually exercised this type. Point subject at the right type and add
    coverage for name validation and ensurable.

--- spec/unit/puppet/type/postgresql_replication_slot_spec.rb
+++ spec/unit/puppet/type/postgresql_replication_slot_spec.rb
@@ -4,10 +4,27 @@ require 'spec_helper'
 
 describe Puppet::Type.type(:postgresql_replication_slot) do
   subject do
-    Puppet::Type.type(:postgresql_psql).new(name: 'standby')
+    described_class.new(name: 'standby1_slot')
   end
 
   it 'has a name parameter' do
-    expect(subject[:name]).to eq 'standby'
+    expect(subject[:name]).to eq 'standby1_slot'
+  end
+
+  it 'accepts a valid slot name' do
+    expect { described_class.new(name: 'standby1_slot') }.not_to raise_error
+  end
+
+  it 'rejects a slot name with invalid characters' do
+    expect { described_class.new(name: 'Standby-1') }.to raise_error(Puppet::Error, %r{invalid}i)
+  end
+
+  it 'is ensurable' do
+    expect(subject.property(:ensure)).not_to be_nil
+  end
+
+  it 'accepts ensure => present and ensure => absent' do
+    expect { described_class.new(name: 'standby1_slot', ensure: :present) }.not_to raise_error
+    expect { described_class.new(name: 'standby1_slot', ensure: :absent) }.not_to raise_error
   end
 end
